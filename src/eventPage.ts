cd17bfdb0fcfd57210d70385b95e6444fc68732c
Harry Liu <2411mail@gmail.com>
2020-03-20 21:55:43 +0800

    add receive msg

--- src/eventPage.ts
+++ src/eventPage.ts
@@ -1,49 +1,7 @@
-/**
- * Retrieves opened tabs list
- * @param {Function} callback
- */
-function getOpenedTabs(callback) {
-    var params = {
-        windowId: chrome.windows.WINDOW_ID_CURRENT
-    };
-    chrome.tabs.query(params, callback);
-}
-
-/**
- * Updates browserAction badge
- * @param {Array} tabs
- */
-function updateBadge(tabs) {
-    chrome.browserAction.setBadgeText({
-        text: String(tabs.length)
-    });
-}
 
 /**
  * Listen messages from popup
  */
-chrome.runtime.onMessage.addListener(function (data, sender, sendResponse) {
-    if (data === 'get-tabs') {
-        getOpenedTabs(sendResponse);
-        return true;
-    }
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  console.log('data: ', request)
 });
-
-/**
- * listen to new tab creation and update badge counter
- */
-chrome.tabs.onCreated.addListener(function () {
-    getOpenedTabs(updateBadge);
-});
-
-/**
- * listen tab onRemoved and update badge counter
- */
-chrome.tabs.onRemoved.addListener(function () {
-    getOpenedTabs(updateBadge);
-});
-
-/**
- * update badge counter on startup
- */
-getOpenedTabs(updateBadge);
