e16a753de78fe4b645ecb9120323c88c2a47357d
Harry Liu <2411mail@gmail.com>
2020-03-20 11:15:26 +0800

    refactor component with react hook

--- src/popup/Popup.tsx
+++ src/popup/Popup.tsx
@@ -4,34 +4,38 @@ import Input from '../Components/Input'
 import Title from '../Components/Title'
 import Textarea from '../Components/Textarea'
 import { Button } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 
-interface AppProps {}
+const useStyles = makeStyles({
+    root: {
+        background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+        border: 0,
+        borderRadius: 3,
+        boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+        color: 'white',
+        height: 48,
+        padding: '0 30px',
+    },
+});
 
-interface AppState {}
 
-export default class Popup extends React.Component<AppProps, AppState> {
-    constructor(props: AppProps, state: AppState) {
-        super(props, state);
-    }
+interface AppProps { }
 
-    componentDidMount() {
-        // Example of how to send a message to eventPage.ts.
-        // chrome.runtime.sendMessage({ popupMounted: true });
-    }
+interface AppState { }
 
-    onChange() {
+export default function(props: AppProps, state: AppState) {
+    function onChange() {
         console.log('onchange here')
     }
 
-    render() {
-        return (
-            <div className="popupContainer">
-                <Title title='Import Mnemonic' />
-                <Textarea onChange={this.onChange} />
-                <Input />
-                <Input />
-                <Button color="primary">Import</Button>
-            </div>
-        )
-    }
+    const classes = useStyles();
+    return (
+        <div className="popupContainer">
+            <Title title='Import Mnemonic' />
+            <Textarea onChange={onChange} />
+            <Input />
+            <Input />
+            <Button className={classes.root} color="primary">Import</Button>
+        </div>
+    )
 }
