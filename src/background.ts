c0d7c3b8c7dc23a9d8f1e550172f62b3ecc3106c
liushooter <shooter.hero@gmail.com>
2020-03-26 22:28:42 +0800

    save current wallet to chrome storage

--- src/background.ts
+++ src/background.ts
@@ -1,33 +1,29 @@
 import { MESSAGE_TYPE } from './utils/constants'
-import {mnemonicToSeedSync } from './wallet/mnemonic';
-import { ExtendedPrivateKey } from "./wallet/key";
+import { mnemonicToSeedSync } from './wallet/mnemonic';
 import Keystore from './wallet/keystore';
 import Keychain from './wallet/keychain';
+
+import { AccountExtendedPublicKey, ExtendedPrivateKey } from "./wallet/key";
+import { AddressType, AddressPrefix } from './wallet/address';
 
 /**
  * Listen messages from popup
  */
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  console.log('data: ', request)
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 
   if (request.messageType === MESSAGE_TYPE.IMPORT_MNEMONIC) {
     // call import mnemonic method
-    console.log(request.mnemonic)
-    console.log(request.password)
-    console.log(request.confirmPassword)
+    const mnemonic = request.mnemonic.trim();
+    const password = request.password.trim();
+    const confirmPassword = request.confirmPassword.trim();
 
-    const mnemonic = request.mnemonic;
-    const password= request.password;
-    const confirmPassword = request.confirmPassword;
-
-    const words = mnemonic.split(" ");
-
-    if(password != confirmPassword) {
+    if (password != confirmPassword) {
       // TODO
       // return err
     }
 
-    if(words.length != 12){ // 12 15 18 24
+    const words = mnemonic.split(" ");
+    if (words.length != 12) { // 12 15 18 24
       // TODO
       // err
     }
@@ -41,13 +37,29 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     const extendedKey = new ExtendedPrivateKey(
       masterKeychain.privateKey.toString('hex'),
       masterKeychain.chainCode.toString('hex')
-  )
+    )
+
     const keystore = Keystore.create(extendedKey, password);
 
+    const accountKeychain = masterKeychain.derivePath(AccountExtendedPublicKey.ckbAccountPath);
+
+    const accountExtendedPublicKey = new AccountExtendedPublicKey(
+      accountKeychain.publicKey.toString('hex'),
+      accountKeychain.chainCode.toString('hex'),
+    )
+
+    // 判断 AddressPrefix
+    const currAddress = accountExtendedPublicKey.address(AddressType.Receiving, 0, AddressPrefix.Mainnet);
+    console.log('currAddress: ' + JSON.stringify(currAddress));
+
+    // TODO
+    // 用户多地址如何保存
+    const wallet = { keystore: keystore.crypto, address: currAddress.address, path: currAddress.path, pubKey: currAddress.publicKey }
+
     chrome.storage.sync.set({
-      key: keystore.crypto
+      "wallet": wallet
     }, () => {
-      console.log('Value is set to ' + JSON.stringify(keystore));
+      console.log('keystore json value: ' + JSON.stringify(keystore));
     });
 
   }
