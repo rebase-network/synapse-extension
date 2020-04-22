d9ed3937818f65f1e85e4a4818d632873592e09a
syuukawa <syuukawa@hotmail.com>
2020-04-22 16:35:18 +0800

    mod MyAddress UI

--- src/ui/pages/MyAddresses/index.tsx
+++ src/ui/pages/MyAddresses/index.tsx
@@ -16,6 +16,7 @@ import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 import ListItemText from '@material-ui/core/ListItemText';
 import ListSubheader from '@material-ui/core/ListSubheader';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 
 const useStyles = makeStyles({
   container: {
@@ -32,17 +33,65 @@ const useStyles = makeStyles({
 const useStyles02 = makeStyles((theme: Theme) =>
   createStyles({
     root: {
-      flexGrow: 1,
-      maxWidth: 600,
+      width: '100%',
+      maxWidth: 400,
+      // backgroundColor: theme.palette.background.paper,
     },
     demo: {
-      backgroundColor: theme.palette.background.paper,
+      // backgroundColor: theme.palette.background.paper,
     },
     title: {
       margin: theme.spacing(1, 0, 1),
     },
+    ListSubheader: {
+      font:'10px',
+      height:'4px',
+      inlineSize: '4px',
+    }
   }),
 );
+
+const subheaderTheme = createMuiTheme({
+  overrides: {
+    // Style sheet name ⚛️
+    MuiListSubheader: {
+      // Name of the rule
+      root: {
+        // Some CSS
+        // background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+        // borderRadius: 3,
+        border: 0,
+        color: 'black',
+        height: '8px',
+        padding: '0 3px',
+        lineHeight: '8px',
+        marginTop:'4px',
+        marginBottom:'4px',
+      },
+    },
+  },
+});
+
+const listItemTheme = createMuiTheme({
+  overrides: {
+    // Style sheet name ⚛️
+    MuiListItem: {
+      // Name of the rule
+      root: {
+        // Some CSS
+        // background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+        // borderRadius: 3,
+        border: 0,
+        color: 'black',
+        height: '6px',
+        padding: '0 13px',
+        lineHeight: '6px',
+        marginTop: '16px',
+        marginBottom: '4px',
+      },
+    },
+  },
+});
 
 interface AppProps { }
 
@@ -84,29 +133,34 @@ export default function (props: AppProps, state: AppState) {
 
   const addressesElem = addresses.map((item, index) => {
     return (
-      <div>
-          <div className="address" data-testid="address">
-            {item.address}
-          </div>
-          <div className="capacity" data-testid="capacity">
-            {item.capacity} CKB
-          </div>
-          <div className="type" data-testid="type">
-            {item.type}
-          </div>
-      </div>
+      // <div>
+      //     <div className="address" data-testid="address">
+      //       {item.address}
+      //     </div>
+      //     <div className="capacity" data-testid="capacity">
+      //       {item.capacity} CKB
+      //     </div>
+      //     <div className="type" data-testid="type">
+      //       {item.type}
+      //     </div>
+      // </div>
       // <ListSubheader>{ item.address }</ListSubheader>
       // <ListItem>
       //   <ListItemText primary= {item.type} secondary= {item.capacity + "CKB"} />
       // </ListItem>
       // <li key={`section-${item.address}`} >
-      //     <ul >
-      //       <ListSubheader>{ item.address }</ListSubheader>
-      //       <ListItem key={`item-${item.address}`}>
-      //           <ListItemText primary= {item.type} secondary= {item.capacity + "CKB"} />
-      //       </ListItem>
-      //     </ul>
-      // </li>
+          <List key={`item-${item.address}`} className={classes02.root} >
+            <ThemeProvider theme={subheaderTheme}>
+                <ListSubheader >{ item.address }</ListSubheader>
+            </ThemeProvider>
+            <ThemeProvider theme={listItemTheme}>
+              <ListItem key={`item-${item.address}`} >
+                  <ListItemText primary= {item.capacity + "  CKB"} secondary= {item.type}/>
+                  {/* <ListItemText secondary= {item.type} /> */}
+              </ListItem>
+            </ThemeProvider>
+          </List>
+      /* </li> */
     )
   })
 
