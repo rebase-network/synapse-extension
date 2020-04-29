0523156eb688d907b3c4883e0dc270f1a769b188
syuukawa <syuukawa@hotmail.com>
2020-04-29 11:38:08 +0800

    add  keyperks.test.ts

---
+++ src/test/keyperKs.test.ts
@@ -0,0 +1,16 @@
+
+
+const keyperwalletTest = require('../keyper/keyperwallet');
+
+const privateKey = "448ff179b923f0602a00f68f23cb8425d30198446a1b5aa2a016deea2762b1f8";
+const password = "123456";
+
+describe('keyper ks test', () => {
+    it('keyper ks', async() => {
+
+        jest.setTimeout(150000)
+
+        await keyperwalletTest.init();
+        await keyperwalletTest.generateByPrivateKey(privateKey, password);
+    })
+  })
