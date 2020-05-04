e73fbb227765fedf6a1640dab4166f43a0e41e83
liushooter <shooter.hero@gmail.com>
2020-05-04 23:13:43 +0800

    change api url

--- src/utils/address.ts
+++ src/utils/address.ts
@@ -2,7 +2,8 @@ import Axios, { AxiosRequestConfig } from "axios";
 
 export const getBalanceByAddress = async (address: string): Promise<number> => {
   // call api
-  const result = await Axios.get(`http://localhost:3000/cell/getBalanceByAddress/${address}`);
+  let url = "http://101.200.147.143:2333/cell/getBalanceByAddress"
+  const result = await Axios.get(url + `/${address}`);
   console.log('result: ', result)
   return result.data;
 }
