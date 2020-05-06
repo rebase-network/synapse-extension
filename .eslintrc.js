7e7d60393698a43538afb893c36f1073293ae9db
Harry Liu <2411mail@gmail.com>
2020-05-06 14:29:44 +0800

    add poetic

---
+++ .eslintrc.js
@@ -0,0 +1,5 @@
+module.exports = {
+  extends: ["./node_modules/poetic/config/eslint/eslint-config.js"],
+  // Add custom rules here
+  rules: {}
+};
---
+++ .prettierrc.js
@@ -0,0 +1,7 @@
+const { prettierConfig } = require("poetic");
+
+module.exports = {
+  ...prettierConfig,
+  // Add custom rules here
+  // printWidth: 100,
+};
