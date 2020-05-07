455cb937cb56e35bfaed3ecb7d32713f0990fc3f
syuukawa <syuukawa@hotmail.com>
2020-05-07 12:31:19 +0800

    modify the UX Address/index.tsx

--- src/ui/pages/Address/index.tsx
+++ src/ui/pages/Address/index.tsx
@@ -2,7 +2,18 @@ import * as React from 'react';
 import { Button, Dialog, IconButton } from '@material-ui/core';
 import CloseIcon from '@material-ui/icons/Close';
 import { useHistory } from 'react-router-dom';
-import { makeStyles } from '@material-ui/core/styles';
+import {
+  createMuiTheme,
+  createStyles,
+  withStyles,
+  makeStyles,
+  Theme,
+  ThemeProvider,
+} from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import TextField from '@material-ui/core/TextField';
+import Divider from '@material-ui/core/Divider';
 import { shannonToCKBFormatter } from '../../../utils/formatters';
 import Title from '../../Components/Title';
 import { MESSAGE_TYPE } from '../../../utils/constants';
@@ -38,14 +49,83 @@ const useStyles = makeStyles({
   }
 });
 
+const useStylesTheme = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      '& > *': {
+        margin: theme.spacing(1),
+      },
+    },
+    paper: {
+      padding: theme.spacing(2),
+      textAlign: 'center',
+      color: theme.palette.text.primary,
+      fontSize: 18,
+      border: 0,
+    },
+  }),
+);
+
+const BootstrapButton = withStyles({
+  root: {
+    width: "88px",
+    size: 'medium',
+    boxShadow: 'none',
+    textTransform: 'none',
+    fontSize: 16,
+    padding: '8px 12px',
+    border: '1px solid',
+    lineHeight: 1.5,
+    backgroundColor: '#0063cc',
+    borderColor: '#0063cc',
+    fontFamily: [
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
+    '&:hover': {
+      backgroundColor: '#0069d9',
+      borderColor: '#0062cc',
+      boxShadow: 'none',
+    },
+    '&:active': {
+      boxShadow: 'none',
+      backgroundColor: '#0062cc',
+      borderColor: '#005cbf',
+    },
+    '&:focus': {
+      boxShadow: '0 0 0 0.2rem rgba(0,123,255,.5)',
+    },
+  },
+})(Button);
+
+const useStylesButton = makeStyles((theme: Theme) =>
+  createStyles({
+    margin: {
+      margin: theme.spacing(1),
+    },
+  }),
+);
+
 interface AppProps { }
 
 interface AppState { }
 
 export default function (props: AppProps, state: AppState) {
   const classes = useStyles();
+  const classesTheme = useStylesTheme();
+  const classesButton = useStylesButton();
+
   const [loading, setLoading] = React.useState(true);
   const [address, setAddress] = React.useState("");
+  const [addressShort, setAddressShort] = React.useState("");
   const [balance, setBalance] = React.useState(0);
   const [open, setOpen] = React.useState(false);
   const [tooltip, setTooltip] = React.useState('');
@@ -61,6 +141,9 @@ export default function (props: AppProps, state: AppState) {
       if (msg.messageType === MESSAGE_TYPE.ADDRESS_INFO) {
         if (msg.address) {
           setAddress(msg.address);
+          const address = msg.address;
+          const addressShort = address.substr(0,10) +"..." + address.substr(address.length-10,address.length);
+          setAddressShort(addressShort);
         } else {
           history.push('./import-mnemonic');
         }
@@ -115,38 +198,51 @@ export default function (props: AppProps, state: AppState) {
 
   return (
     <div className={classes.container}>
-      <Title title="Address" testId="address-title" />
-      <div className="address" data-testid="address-info">
-        {address}
-      </div>
-      {balanceNode}
-      <div className="">
-        <Button
-          type="button"
-          id="receive-button"
-          color="primary"
-          variant="contained"
-          className={classes.button}
-          data-testid="receive"
-          onClick={handleClickOpen}
-        >
-          Receive
-        </Button>
+      <div className="classesTheme.root">
+        <Grid container spacing={2}>
+            <Grid item xs={12} alignContent="center" alignItems="center">
+              <Paper className={classesTheme.paper}>Address</Paper>
+            </Grid>
+            <Grid item xs={12}>
+              <Paper className={classesTheme.paper}>{addressShort}</Paper>
+            </Grid>
+        </Grid>
 
-        <br />
-        <br />
-
-        <Button
-          id="send-button"
-          color="primary"
-          onClick={onSendtx}
-          variant="contained"
-          className={classes.button}
-          data-testid="send"
-        >
-          Send
-        </Button>
+        <Grid container spacing={3}>
+            <Grid item xs={12}>
+              <Paper className={classesTheme.paper}>{balanceNode}</Paper>
+            </Grid>
+            <Grid item xs={6} sm={3} alignItems="center" alignContent="center">
+              <BootstrapButton
+                  type="button"
+                  id="receive-button"
+                  color="primary"
+                  variant="contained"
+                  className={classesButton.margin}
+                  data-testid="receive"
+                  onClick={handleClickOpen}
+                >
+                Receive
+              </BootstrapButton>
+            </Grid>
+            <Grid item xs={6} sm={3} alignItems="center" alignContent="center">
+              <BootstrapButton
+                  id="send-button"
+                  color="primary"
+                  onClick={onSendtx}
+                  variant="contained"
+                  className={classesButton.margin}
+                  data-testid="send"
+                >
+                Send
+              </BootstrapButton>
+            </Grid>
+        </Grid>
       </div>
+      <br/>
+      <br/>
+      <Divider variant="middle" />
+
       <Dialog open={open}>
         <div className={classes.dialogTitle}>
           <IconButton onClick={handleClose}>
