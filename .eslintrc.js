00219b01462ad86653adc6a5c8bec5bd6ac28fff
Harry Liu <2411mail@gmail.com>
2020-05-09 01:40:18 +0800

    Revert "add poetic"
    This reverts commit 7e7d60393698a43538afb893c36f1073293ae9db.

--- .eslintrc.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  extends: ["./node_modules/poetic/config/eslint/eslint-config.js"],
-  // Add custom rules here
-  rules: {}
-};
--- .prettierrc.js
+++
@@ -1,7 +0,0 @@
-const { prettierConfig } = require("poetic");
-
-module.exports = {
-  ...prettierConfig,
-  // Add custom rules here
-  // printWidth: 100,
-};
