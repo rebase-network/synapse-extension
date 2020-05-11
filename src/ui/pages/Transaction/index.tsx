4721446227b281d7c9eeb39a5a3df3459a6b4a16
Harry Liu <2411mail@gmail.com>
2020-05-11 12:44:47 +0800

    format code

--- src/ui/pages/Transaction/index.tsx
+++ src/ui/pages/Transaction/index.tsx
@@ -1,30 +1,27 @@
-import * as React from 'react';
-import Title from '../../Components/Title'
-import { Button, TextField } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
+import * as React from "react";
+import { Link } from "react-router-dom";
+import Title from "../../Components/Title";
+import { Button, TextField } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 import { Formik, Form } from "formik";
 import * as Yup from "yup";
-import { MESSAGE_TYPE } from '../../../utils/constants'
+import { MESSAGE_TYPE } from "../../../utils/constants";
 import { useHistory } from "react-router-dom";
-import { AppContext } from '../../App';
+import { AppContext } from "../../App";
 
 const useStyles = makeStyles({
   container: {
     margin: 30,
   },
-  button: {
-
-  },
-  textField: {
-
-  }
+  button: {},
+  textField: {},
 });
 
-interface AppProps { }
+interface AppProps {}
 
-interface AppState { }
+interface AppState {}
 
-export const innerForm = props => {
+export const innerForm = (props) => {
   const classes = useStyles();
   const {
     values,
@@ -36,12 +33,11 @@ export const innerForm = props => {
     handleChange,
     handleBlur,
     handleSubmit,
-    handleReset
+    handleReset,
   } = props;
 
   return (
     <Form className="form-mnemonic" id="form-mnemonic" onSubmit={handleSubmit}>
-
       <TextField
         label="ToAddress"
         name="address"
@@ -129,16 +125,20 @@ export const innerForm = props => {
       </Button>
     </Form>
   );
-}
+};
 
 export default function (props: AppProps, state: AppState) {
-  const [success, setSuccess] = React.useState(false)
+  const [success, setSuccess] = React.useState(false);
   const history = useHistory();
   const { network } = React.useContext(AppContext);
   const [valAddress, setValAddress] = React.useState(true);
 
   React.useEffect(() => {
-    chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+    chrome.runtime.onMessage.addListener(function (
+      message,
+      sender,
+      sendResponse
+    ) {
       //跳转到交易详细信息的页面
       if (message.messageType === MESSAGE_TYPE.TO_TX_DETAIL) {
         console.log("TO_TX_DETAIL message", JSON.stringify(message));
@@ -146,14 +146,14 @@ export default function (props: AppProps, state: AppState) {
         //001-传递交易信息
         chrome.runtime.sendMessage({
           message,
-          messageType: MESSAGE_TYPE.REQUEST_TX_DETAIL
-        })
+          messageType: MESSAGE_TYPE.REQUEST_TX_DETAIL,
+        });
         //002-跳转到页面
-        history.push('/tx-detail')
+        history.push("/tx-detail");
       }
-    })
+    });
     // setLoading(true);
-  }, [])
+  }, []);
 
   const onSubmit = async (values) => {
     console.log("onSubmit=>", values);
@@ -165,10 +165,13 @@ export default function (props: AppProps, state: AppState) {
 
     // 消息发送到Background.ts
     // network - TODO
-    chrome.runtime.sendMessage({ ...values, network, messageType: MESSAGE_TYPE.RESQUEST_SEND_TX })
-    setSuccess(true)
-
-  }
+    chrome.runtime.sendMessage({
+      ...values,
+      network,
+      messageType: MESSAGE_TYPE.RESQUEST_SEND_TX,
+    });
+    setSuccess(true);
+  };
 
   //check the current network and address
   const validateAddress = (address, network) => {
@@ -176,45 +179,40 @@ export default function (props: AppProps, state: AppState) {
       setValAddress(false);
       return;
     }
-    if (network == "testnet" && !address.startsWith('ckt')) {
+    if (network == "testnet" && !address.startsWith("ckt")) {
       setValAddress(false);
       return;
     }
-    if (network == "mainnet" && !address.startsWith('ckb')) {
+    if (network == "mainnet" && !address.startsWith("ckb")) {
       setValAddress(false);
       return;
     }
-  }
+  };
 
-  let successNode = null
-  if (success) successNode = <div className="success">Successfully</div>
-  let validateNode = null
-  if (!valAddress) validateNode = <div className="success">Invalid Address</div>
-
+  let successNode = null;
+  if (success) successNode = <div className="success">Successfully</div>;
+  let validateNode = null;
+  if (!valAddress)
+    validateNode = <div className="success">Invalid Address</div>;
 
   const classes = useStyles();
   return (
     <div className={classes.container}>
-      <Title title='Send CKB' testId="sendtx-form-title" />
+      <Title title="Send CKB" testId="sendtx-form-title" />
       {successNode}
       {validateNode}
       <Formik
         initialValues={{ address: "", amount: "", fee: "", password: "" }}
-
         onSubmit={onSubmit}
         validationSchema={Yup.object().shape({
-          address: Yup.string()
-            .required("Required"),
-          amount: Yup.string()
-            .required("Required"),
-          fee: Yup.string()
-            .required("Required"),
-          password: Yup.string()
-            .required("Required"),
+          address: Yup.string().required("Required"),
+          amount: Yup.string().required("Required"),
+          fee: Yup.string().required("Required"),
+          password: Yup.string().required("Required"),
         })}
       >
         {innerForm}
       </Formik>
     </div>
-  )
+  );
 }
