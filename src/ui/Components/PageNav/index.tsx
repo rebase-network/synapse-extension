f86efd2bc907e6c047af53d4efe66bfb06d2c438
Harry Liu <2411mail@gmail.com>
2020-05-12 19:59:56 +0800

    add right nav to PageNav component

--- src/ui/Components/PageNav/index.tsx
+++ src/ui/Components/PageNav/index.tsx
@@ -5,6 +5,7 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import NavigateBeforeIcon from '@material-ui/icons/NavigateBefore';
+import NavigateNextIcon from '@material-ui/icons/NavigateNext';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -14,30 +15,54 @@ const useStyles = makeStyles((theme: Theme) =>
     menuButton: {
       marginRight: theme.spacing(2),
     },
+    menuButtonRight: {
+      marginRight: theme.spacing(2),
+      position: 'absolute',
+      right: 10,
+    },
   }),
 );
 
 interface AppProps {
   to: string;
   title: string;
+  position?: string;
+  onClickRight?: Function;
 }
 
-export default function DenseAppBar(props: AppProps) {
+export default function PageNav(props: AppProps) {
   const classes = useStyles();
-  const navButton = props.to ? (
-    <IconButton edge="start" className={classes.menuButton} aria-label="nav">
-      <Link to={props.to}>
-        <NavigateBeforeIcon />
-      </Link>
-    </IconButton>
-  ) : (
-    ''
-  );
+  let navButtonBefore;
+  let navButtonNext;
+  if (props.to && props.position !== 'right') {
+    navButtonBefore = (
+      <IconButton edge="start" className={classes.menuButton} aria-label="nav">
+        <Link to={props.to}>
+          <NavigateBeforeIcon />
+        </Link>
+      </IconButton>
+    );
+  } else if (props.position === 'right') {
+    navButtonNext = (
+      <IconButton
+        edge="start"
+        className={classes.menuButtonRight}
+        aria-label="nav"
+        onClick={() => props.onClickRight('right', false)}
+      >
+        <Link to={props.to}>
+          <NavigateNextIcon />
+        </Link>
+      </IconButton>
+    );
+  }
+
   return (
     <div className={classes.root}>
       <Toolbar>
-        {navButton}
+        {navButtonBefore}
         <Typography variant="h6">{props.title}</Typography>
+        {navButtonNext}
       </Toolbar>
     </div>
   );
