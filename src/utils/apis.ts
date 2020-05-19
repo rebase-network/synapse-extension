1039ba50bfec61dde285c8aab8a52fdffbac362d
Harry Liu <2411mail@gmail.com>
2020-05-19 15:51:22 +0800

    update api

--- src/utils/apis.ts
+++ src/utils/apis.ts
@@ -1,9 +1,9 @@
 import Axios, { AxiosRequestConfig } from 'axios';
 import { Ckb } from './constants';
 
-export const getAddressInfo = async (address: string): Promise<{ capacity: string }> => {
+export const getAddressInfo = async (lockHash: string): Promise<{ capacity: string }> => {
   // call api
-  const result = await Axios.get(`${Ckb.testnetApiUrl}/address/${address}`);
+  const result = await Axios.get(`${Ckb.testnetApiUrl}/address/${lockHash}`);
   console.log('getAddressInfo result: ', result);
   return result.data;
 };
