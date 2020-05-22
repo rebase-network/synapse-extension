2d724d22bf6dd9af366e72304a7d6518cd0f948e
Harry Liu <2411mail@gmail.com>
2020-05-22 16:12:34 +0800

    use modal to render tx detail

--- src/ui/Components/TxList/index.tsx
+++ src/ui/Components/TxList/index.tsx
@@ -3,40 +3,15 @@ import * as moment from 'moment';
 import * as _ from 'lodash';
 import { ListItem, ListItemText, List, Link, Tooltip } from '@material-ui/core';
 import CallMadeIcon from '@material-ui/icons/CallMade';
-import CloseIcon from '@material-ui/icons/Close';
-import { useHistory } from 'react-router-dom';
 import { createStyles, withStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import Divider from '@material-ui/core/Divider';
 import { EXPLORER_URL } from '../../../utils/constants';
+import Modal from '../Modal';
+import TxDetail from '../TxDetail';
 
 const useStyles = makeStyles({
-  container: {
-    margin: 30,
-  },
-  button: {
-    textAlign: 'center',
-  },
-  dialogTitle: {
-    textAlign: 'right',
-  },
-  dialogContent: {
-    padding: '0 16px 24px',
-    textAlign: 'center',
-    minWidth: 200,
-  },
-  address: {
-    marginTop: 16,
-    fontSize: 12,
-    'word-break': 'break-all',
-  },
-  loading: {
-    width: 200,
-    padding: 24,
-    textAlign: 'center',
-  },
-  tip: {
-    marginBottom: 24,
-    color: 'green',
+  list: {
+    cursor: 'pointer',
   },
 });
 
@@ -45,16 +20,28 @@ interface AppProps {
 }
 
 export default function (props: AppProps) {
-  const onTxDetail = () => {
-    return;
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+  const [selectedTxHash, setSelectedTxHash] = React.useState('');
+
+  const toggleModal = () => {
+    setOpen(!open);
+  };
+
+  const closeModal = () => {
+    setSelectedTxHash('');
+  };
+
+  const onSelectTx = (hash) => {
+    toggleModal();
+    setSelectedTxHash(hash);
   };
 
   const txListElem = props.txList.map((item) => (
-    <List onClick={onTxDetail}>
+    <List onClick={() => onSelectTx(item.hash)} key={item.hash} className={classes.list}>
       <Divider />
       <ListItem>
-        <ListItemText primary={moment(item.timestamp).format('YYYY-MM-DD HH:mm:ss')} />
-
+        <ListItemText primary={`${item.amount / 10 ** 8} CKB`} />
         <Link rel="noreferrer" target="_blank" href={EXPLORER_URL + item.hash}>
           <Tooltip title="View on Explorer" placement="top">
             <CallMadeIcon />
@@ -62,11 +49,14 @@ export default function (props: AppProps) {
         </Link>
       </ListItem>
       <ListItem>
-        <ListItemText secondary={`${item.amount / 10 ** 8} CKB`} />
-        <ListItemText secondary={item.income ? `Received` : `Send`} />
+        <ListItemText secondary={item.income ? `Received` : `Sent`} />
+        <ListItemText secondary={moment(item.timestamp).format('YYYY-MM-DD HH:mm:ss')} />
       </ListItem>
+      <Modal open={open && selectedTxHash === item.hash} onClose={closeModal}>
+        <TxDetail data={item} />
+      </Modal>
     </List>
   ));
 
-  return txListElem;
+  return <div>{txListElem}</div>;
 }
