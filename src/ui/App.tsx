f60da59587b3cf7667d0ee21347a2a778e4ba1ae
Harry Liu <2411mail@gmail.com>
2020-05-22 16:12:09 +0800

    move tx detail from page to component level

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -5,7 +5,6 @@ import Address from './pages/Address';
 import ImportMnemonic from './pages/ImportMnemonic/index';
 import GenerateMnemonic from './pages/ImportMnemonic/generate';
 import Transaction from './pages/Transaction';
-import TxDetail from './pages/TxDetail';
 import TxHistoryDetail from './pages/Transaction/txHistoryDetail';
 import MnemonicSetting from './pages/MnemonicSetting';
 import ImportPrivateKey from './pages/ImportPrivateKey';
@@ -53,9 +52,6 @@ export default function App() {
             </Route>
             <Route path="/tx-history-detail">
               <TxHistoryDetail />
-            </Route>
-            <Route path="/tx-detail">
-              <TxDetail />
             </Route>
             <Route path="/import-private-key">
               <ImportPrivateKey />
--- src/ui/Components/TxDetail/index.tsx
+++ src/ui/Components/TxDetail/index.tsx
@@ -1,80 +1,64 @@
 import * as React from 'react';
-
+import * as moment from 'moment';
 import { Link } from '@material-ui/core';
-import { MESSAGE_TYPE } from '../../../utils/constants';
 import { makeStyles } from '@material-ui/core/styles';
-
 import Box from '@material-ui/core/Box';
 import SyncIcon from '@material-ui/icons/Sync';
 import Grid from '@material-ui/core/Grid';
 import Divider from '@material-ui/core/Divider';
 import CheckCircleIcon from '@material-ui/icons/CheckCircle';
 import Typography from '@material-ui/core/Typography';
-
-import PageNav from '../../Components/PageNav';
+import PageNav from '../PageNav';
 
 const useStyles = makeStyles((theme) => ({
-  container: {
-    margin: 30,
-  },
-  button: {},
-  textField: {},
   root: {
     flexGrow: 1,
     overflow: 'hidden',
     margin: `${theme.spacing(1)}px auto`,
-    marginTop: '20',
-    marginBottom: '20',
-    // padding: theme.spacing(0, 3),
+    padding: 15,
   },
   divider: {
-    marginTop: '20',
-    marginBottom: '20',
+    marginTop: 20,
+    marginBottom: 20,
   },
   typography: {
-    overflowWrap: "anywhere",
-  }
+    overflowWrap: 'anywhere',
+  },
 }));
 
-interface AppProps { }
+interface AppProps {
+  data: any;
+}
 
-interface AppState { }
+interface AppState {}
 
 export default function (props: AppProps, state: AppState) {
   const classes = useStyles();
 
-  const [status, setStatus] = React.useState('');
-  const [capacity, setCapacity] = React.useState(0);
-  const [fee, setFee] = React.useState(0);
-  const [inputs, setInputs] = React.useState('');
-  const [outputs, setOutputs] = React.useState('');
-  const [txHash, setTxHash] = React.useState('');
-  const [block, setBlock] = React.useState('');
-  const [txDateTime, setTxDateTime] = React.useState("");
+  const {
+    status = 'Confirmed',
+    amount: capacity,
+    fee,
+    inputs,
+    outputs,
+    hash: txHash,
+    blockNum,
+    timestamp,
+  } = props.data;
 
-  React.useEffect(() => {
-    chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-      if (message.messageType === MESSAGE_TYPE.TX_DETAIL) {
-        setCapacity(message.capacity / (10 ** 8));
-        setFee(message.fee / (10 ** 8));
-        setInputs(message.inputs);
-        setOutputs(message.outputs);
-        setTxHash(message.txHash);
-        setStatus(message.status);
-        setBlock(message.blockNumber);
-        const dateTimeStr = new Date().toLocaleDateString() + " " + new Date().toLocaleTimeString();
-        setTxDateTime(dateTimeStr);
-      }
-    });
-  }, []);
+  const txDateTime = moment(timestamp).format('YYYY-MM-DD HH:mm:ss');
 
-  let statusIconNode = <Box textAlign="center" fontSize={40}>
-    <SyncIcon />
-  </Box>;
-  if (status == "finished") {
-    statusIconNode = <Box textAlign="center" fontSize={40}>
-      <CheckCircleIcon />
+  let statusIconNode = (
+    <Box textAlign="center" fontSize={40}>
+      <SyncIcon />
     </Box>
+  );
+  if (status == 'finished') {
+    statusIconNode = (
+      <Box textAlign="center" fontSize={40}>
+        <CheckCircleIcon />
+      </Box>
+    );
   }
   return (
     <div>
@@ -82,10 +66,7 @@ export default function (props: AppProps, state: AppState) {
       <div className={classes.root}>
         <Grid container spacing={3}>
           <Grid item xs={12}>
-            {/* <Box textAlign="center" fontSize={40}>
-                  <SyncIcon/>
-              </Box> */}
-            {statusIconNode}
+            {/* {statusIconNode} */}
             <Box textAlign="center" fontSize={22}>
               {status}
             </Box>
@@ -102,18 +83,20 @@ export default function (props: AppProps, state: AppState) {
             <Typography noWrap>&nbsp;&nbsp;Capacity&nbsp;&nbsp;</Typography>
           </Grid>
           <Grid item xs>
-            <Typography>{capacity} CKB</Typography>
+            <Typography>{capacity / 10 ** 8} CKB</Typography>
           </Grid>
         </Grid>
         <Grid container wrap="nowrap" spacing={2}>
           <Grid item>
-            <Typography noWrap>&nbsp;&nbsp;Fee&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</Typography>
+            <Typography noWrap>
+              &nbsp;&nbsp;Fee&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            </Typography>
           </Grid>
           <Grid item xs>
-            <Typography>{fee} CKB </Typography>
+            <Typography>{fee / 10 ** 8} CKB </Typography>
           </Grid>
         </Grid>
-        <Grid container wrap="nowrap" spacing={2}>
+        {/* <Grid container wrap="nowrap" spacing={2}>
           <Grid item>
             <Typography noWrap>&nbsp;&nbsp;Inputs&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</Typography>
           </Grid>
@@ -128,15 +111,15 @@ export default function (props: AppProps, state: AppState) {
           <Grid item xs>
             <Typography className={classes.typography}>{outputs}</Typography>
           </Grid>
-        </Grid>
+        </Grid> */}
         <br />
         <Divider variant="middle" className={classes.divider} />
         <br />
-        <Grid container wrap="nowrap" spacing={2} >
+        <Grid container wrap="nowrap" spacing={2}>
           <Grid item>
             <Typography noWrap>&nbsp;&nbsp;Tx Hash&nbsp;&nbsp;</Typography>
           </Grid>
-          <Grid item xs >
+          <Grid item xs>
             <Link
               rel="noreferrer"
               target="_blank"
@@ -146,14 +129,14 @@ export default function (props: AppProps, state: AppState) {
             </Link>
           </Grid>
         </Grid>
-        {/* <Grid container wrap="nowrap" spacing={2}>
+        <Grid container wrap="nowrap" spacing={2}>
           <Grid item>
             <Typography noWrap>&nbsp;&nbsp;Block&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</Typography>
           </Grid>
           <Grid item xs>
-            <Typography>{block}</Typography>
+            <Typography>{blockNum}</Typography>
           </Grid>
-        </Grid> */}
+        </Grid>
       </div>
     </div>
   );
