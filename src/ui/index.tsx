961fd418dd68e51552803dae8c49f95d9b00bded
Harry Liu <2411mail@gmail.com>
2020-05-25 18:15:31 +0800

    put ckb inside window for global use

--- src/ui/index.tsx
+++ src/ui/index.tsx
@@ -2,6 +2,20 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
 
+import { configService } from '../config';
+import CKB from '@nervosnetwork/ckb-sdk-core';
+
+
+declare global {
+  interface Window {
+    ckb: any;
+  }
+}
+
+window.ckb = {
+  rpc: new CKB(configService.CKB_RPC_ENDPOINT),
+};
+
 chrome.tabs.query({ active: true, currentWindow: true }, (tab) => {
   ReactDOM.render(<App />, document.getElementById('popup'));
 });
