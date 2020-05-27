d4533a41ef715a9622eded36edb2f1eeedfdedd0
Harry Liu <2411mail@gmail.com>
2020-05-27 13:44:42 +0800

    add jest setup files, add crypto module for jsdom global varible

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   roots: ['<rootDir>/src'],
+  setupFilesAfterEnv: ['./jest.setup.js'],
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
     '^.+\\.scss$': '<rootDir>/config/jest/cssTransform.js'
