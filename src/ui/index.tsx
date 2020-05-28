88c6bccd5ffd9f7c3403a8f0842cbb3bf3693cdb
liushooter <shooter.hero@gmail.com>
2020-05-28 10:45:58 +0800

    set i18n

--- src/ui/index.tsx
+++ src/ui/index.tsx
@@ -1,10 +1,17 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { IntlProvider } from 'react-intl';
+import zh from './pages/locales/zh'
+import en from './pages/locales/en'
+
+let messages = {}
+messages['en'] = en;
+messages['zh'] = zh;
+
 import App from './App';
 
 import { configService } from '../config';
 import CKB from '@nervosnetwork/ckb-sdk-core';
-
 
 declare global {
   interface Window {
@@ -17,5 +24,5 @@ window.ckb = {
 };
 
 chrome.tabs.query({ active: true, currentWindow: true }, (tab) => {
-  ReactDOM.render(<App />, document.getElementById('popup'));
+  ReactDOM.render(<IntlProvider locale={'zh'} messages={messages['zh']} ><App /> </IntlProvider>, document.getElementById('popup'));
 });
