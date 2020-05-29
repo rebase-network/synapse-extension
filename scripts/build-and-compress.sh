4151f821d037ba3b1fdaa6f19914633f26a4b225
Harry Liu <2411mail@gmail.com>
2020-05-29 18:57:05 +0800

    add release command

---
+++ scripts/build-and-compress.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+rm -rf dist synapse-extension
+
+yarn build
+
+mv dist synapse-extension
+
+# 打包压缩
+zip -q -r synapse-extension.zip synapse-extension
+
+mv synapse-extension dist
+
+# 文件的校验和
+shasum synapse-extension.zip
