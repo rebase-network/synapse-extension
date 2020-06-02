a746548fe6f4f462735ed9d485b804ab32a984e8
Harry Liu <2411mail@gmail.com>
2020-06-02 17:29:42 +0800

    dapp could use postMessage for send tx method

--- src/contentScript/inject/injectedScript.ts
+++ src/contentScript/inject/injectedScript.ts
@@ -1,16 +1,34 @@
+import { MESSAGE_TYPE } from '../../utils/constants'
 import { getAddressInfo } from '../../utils/apis';
 import { configService } from '../../config';
 import CKB from '@nervosnetwork/ckb-sdk-core';
 
+interface SignPayload {
+  message: string;
+}
+interface SendPayload {
+  from?: string;
+  to: string;
+  capacity: number;
+  fee?: number;
+}
 // TODO: improve me
 window.ckb = {
-  rpc: new CKB(configService.CKB_RPC_ENDPOINT),
-  getAddressInfo,
-  getNetworkInfo: () => ({
-    network: 'testnetormainnet',
-  }),
-  sign: () => {},
-  send: () => {},
+  // rpc: new CKB(configService.CKB_RPC_ENDPOINT),
+  // getAddressInfo,
+  // sign: (payload: SignPayload) => {
+  //   window.postMessage({
+  //     messageType: MESSAGE_TYPE.EXTERNAL_SIGN,
+  //     payload
+  //   }, '*');
+  // },
+  send: (payload: SendPayload) => {
+    window.postMessage({
+      messageType: MESSAGE_TYPE.EXTERNAL_SEND,
+      payload
+    }, '*');
+  },
 };
 
-console.log('ckb inject successfully');
+console.log('ckb is injected successfully');
+
