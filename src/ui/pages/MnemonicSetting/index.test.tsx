dc17408b5a3161c1809b9c8a951437fe9b976c0f
Harry Liu <2411mail@gmail.com>
2020-06-08 18:49:42 +0800

    fix test err

--- src/ui/pages/MnemonicSetting/index.test.tsx
+++ src/ui/pages/MnemonicSetting/index.test.tsx
@@ -1,28 +1,31 @@
 import * as React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import App from '../../App';
+import App from './index';
 import { render, fireEvent, waitFor, cleanup, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import * as chrome from 'sinon-chrome';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { IntlProvider } from 'react-intl';
+import en from '../locales/en';
 
-describe('Home testing', () => {
-  let tree;
-  beforeEach(() => {
-    tree = render(
-      <MemoryRouter initialEntries={['/']}>
-        <App />
-      </MemoryRouter>,
-    );
-  });
-  afterEach(cleanup);
-
+describe('Mnemonic Setting page', () => {
+  let tree, container, getByTestId;
   beforeAll(() => {
+    tree = render(
+      <IntlProvider locale="en" messages={en}>
+        <Router>
+          <App />
+        </Router>
+      </IntlProvider>,
+    );
+    container = tree.container;
+    getByTestId = tree.getByTestId;
+
     window.chrome = chrome;
   });
 
-  it('should render Import / Generate btn', () => {
-    const { getByTestId, container } = tree;
+  afterAll(cleanup);
 
+  it('should render Import / Generate btn', () => {
     const btn1 = getByTestId('import-button');
     const btn2 = getByTestId('generate-button');
     expect(container).toContainElement(btn1);
