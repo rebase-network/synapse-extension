57bc63765c57cd0e0244340b4616beaab256b57c
liushooter <shooter.hero@gmail.com>
2020-06-08 16:50:39 +0800

    update test case

--- src/background/address.test.ts
+++ src/background/address.test.ts
@@ -3,7 +3,7 @@ import { getAddressInfo } from './address';
 describe('address util test', () => {
   it('should return value for address balance', async () => {
     const address = 'ckt1qyq02llz4hpvl3sz9wkmt7qqh0397x2cdegsem7ykn';
-    const balance = await getAddressInfo(address);
-    expect(balance).toBeGreaterThan(100);
+    const addressInfo = await getAddressInfo(address);
+    expect(typeof addressInfo.capacity).toEqual('string')
   });
 });
--- src/test/addressScript.test.ts
+++ src/test/addressScript.test.ts
@@ -18,12 +18,4 @@ describe('addressToScript', () => {
     );
   });
 
-  //     test("kaccak256 decode", () => {
-  //       const script = addressToScript("ckt1qjjm395fg5uc986703vs9uqzw5gljnrslgjqd4gfulrdrhmkkphs3s7nwu6x3pnl82rz3xmqypfhcway723ngkutufp");
-  //       expect(script).toEqual(expect.objectContaining({
-  //         hashType: "type",
-  //         codeHash: "0xa5b896894539829f5e7c5902f0027511f94c70fa2406d509e7c6d1df76b06f08",
-  //         args: "0xc3d3773468867f3a86289b6020537c3ba4f2a334"
-  //       }));
-  //     });
 });
