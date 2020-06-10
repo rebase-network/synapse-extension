7cc8783aa84f1c84e553d8b4cf93a8f7072bd66f
Harry Liu <2411mail@gmail.com>
2020-06-10 17:25:12 +0800

    fix eslint error

--- webpack.common.js
+++ webpack.common.js
@@ -1,74 +1,81 @@
-const path = require("path");
+const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 
 module.exports = {
   entry: {
-    popup: path.join(__dirname, "src/ui/index.tsx"),
     background: path.join(__dirname, "src/background.ts"),
-    contentScript: path.join(__dirname, "src/contentScript/contentScript.ts"),
-    injectedScript: path.join(__dirname, "src/contentScript/inject/injectedScript.ts"),
+    popup: path.join(__dirname, 'src/ui/index.tsx'),
+    contentScript: path.join(__dirname, 'src/contentScript/contentScript.ts'),
+    injectedScript: path.join(__dirname, 'src/contentScript/inject/injectedScript.ts'),
   },
   output: {
-    path: path.join(__dirname, "dist/js"),
-    filename: "[name].js",
+    path: path.join(__dirname, 'dist/js'),
+    filename: '[name].js',
   },
   plugins: [
-    new CopyPlugin([{
-        from: "./src/manifest.json",
-        to: path.join(__dirname, "dist"),
+    new CopyPlugin([
+      // prettier-ignore
+      {
+        from: './src/manifest.json',
+        to: path.join(__dirname, 'dist'),
       },
       {
-        from: "./src/popup.html",
-        to: path.join(__dirname, "dist"),
+        from: './src/popup.html',
+        to: path.join(__dirname, 'dist'),
       },
       {
-        from: "./src/notification.html",
-        to: path.join(__dirname, "dist"),
+        from: './src/notification.html',
+        to: path.join(__dirname, 'dist'),
       },
       {
-        from: "./src/background.html",
-        to: path.join(__dirname, "dist"),
+        from: './src/background.html',
+        to: path.join(__dirname, 'dist'),
       },
       {
-        from: "./src/images/logo-32.png",
-        to: path.join(__dirname, "dist"),
+        from: './src/images/logo-32.png',
+        to: path.join(__dirname, 'dist'),
       },
       {
-        from: "./src/images/logo-128.png",
-        to: path.join(__dirname, "dist"),
+        from: './src/images/logo-128.png',
+        to: path.join(__dirname, 'dist'),
       },
     ]),
     new Dotenv(),
   ],
   module: {
-    rules: [{
+    rules: [
+      // prettier-ignore
+      {
         exclude: /node_modules/,
         test: /\.tsx?$/,
-        use: "ts-loader"
+        use: 'ts-loader',
       },
       {
         exclude: /node_modules/,
         test: /\.scss$/,
-        use: [{
-            loader: "style-loader" // Creates style nodes from JS strings
+        use: [
+          // prettier-ignore
+          {
+            loader: 'style-loader', // Creates style nodes from JS strings
           },
           {
-            loader: "css-loader" // Translates CSS into CommonJS
+            loader: 'css-loader', // Translates CSS into CommonJS
           },
           {
-            loader: "sass-loader" // Compiles Sass to CSS
-          }
-        ]
-      }
-    ]
+            loader: 'sass-loader', // Compiles Sass to CSS
+          },
+        ],
+      },
+    ],
   },
   resolve: {
-    extensions: [".ts", ".tsx", ".js"],
-    plugins: [new TsconfigPathsPlugin({
-      configFile: "./tsconfig.json"
-    })]
-
-  }
+    extensions: ['.ts', '.tsx', '.js'],
+    plugins: [
+      new TsconfigPathsPlugin({
+        configFile: './tsconfig.json',
+      }),
+    ],
+  },
 };
