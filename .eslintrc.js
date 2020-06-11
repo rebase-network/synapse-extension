353a3716c51d3ecd6c964f8b82a1f17aa55ffa97
Harry Liu <2411mail@gmail.com>
2020-06-11 17:50:39 +0800

    move package to dev dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,4 +6,9 @@ module.exports = {
   parserOptions: {
     project: './tsconfig.json',
   },
+  rules: {
+    "import/no-extraneous-dependencies": ["error", {
+      "devDependencies": true
+    }]
+  }
 };
