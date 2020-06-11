ae8a8e9497f1539dd6af53f03dd8d57b70e98d5b
Harry Liu <2411mail@gmail.com>
2020-06-11 17:54:37 +0800

    clean code

--- src/contentScript/contentScript.ts
+++ src/contentScript/contentScript.ts
@@ -6,10 +6,9 @@ function injectCustomJs(jsPath) {
   const temp = document.createElement('script');
   const container = document.head || document.documentElement;
   temp.setAttribute('type', 'text/javascript');
-  // 获得的地址类似：chrome-extension://ggpbicboegkhkbnoifoljffhicfhgbjn/js/injectedScript.js
-  temp.src = chrome.extension.getURL(jsPathToInject);
+  // full url will be: chrome-extension://ggpbicboegkhkbnoifoljffhicfhgbjn/js/injectedScript.js
+  temp.src = browser.extension.getURL(jsPathToInject);
   temp.onload = () => {
-    // 放在页面不好看，执行完后移除掉
     temp.remove();
   };
   container.appendChild(temp);
