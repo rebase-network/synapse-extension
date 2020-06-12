7072c06ee718545d8dfac0b708a871f83f9f406b
Harry Liu <2411mail@gmail.com>
2020-06-12 15:20:05 +0800

    use common message handler to handle message

--- src/contentScript/inject/injectedScript.ts
+++ src/contentScript/inject/injectedScript.ts
@@ -1,42 +1,68 @@
 import { MESSAGE_TYPE } from '@utils/constants';
 import { BACKGROUND_PORT, WEB_PAGE } from '@utils/message/constants';
 
+interface RequestMessage {
+  type: string;
+  target: string;
+  token: string;
+  requestId: string;
+}
+
+interface TXMeta {
+  from: string;
+  to: string;
+  capacity: number;
+  data: string;
+}
+
+interface SignSendTXRequest {
+  tx: CKBComponents.RawTransactionToSign;
+  meta: TXMeta;
+}
+
+const promisedMessageHandler = (requestMessage: RequestMessage) => {
+  window.postMessage(requestMessage, '*');
+
+  return new Promise((resolve) => {
+    const resultHandler = (e) => {
+      const resultMessage = e.data;
+      const isMessageValid = resultMessage.type && resultMessage.target === WEB_PAGE;
+      if (isMessageValid) {
+        window.removeEventListener('resultMessage', resultHandler);
+        resolve(resultMessage);
+      }
+    };
+    window.addEventListener('message', resultHandler, false);
+  });
+};
+
 window.ckb = {
   // rpc: new CKB(configService.CKB_RPC_ENDPOINT),
-  getAddressInfo: async () => {
-    window.postMessage(
-      {
-        type: MESSAGE_TYPE.EXTERNAL_GET_ADDRESS_INFO,
-        port: BACKGROUND_PORT,
+  getAddressInfo: async (
+    requestMessage: RequestMessage = {
+      type: MESSAGE_TYPE.EXTERNAL_GET_ADDRESS_INFO,
+      target: BACKGROUND_PORT,
+      token: 'getAddressInfoToken',
+      requestId: 'getAddressInfoRequestId',
+    },
+  ) => promisedMessageHandler(requestMessage),
+
+  send: (signSendRequest: SignSendTXRequest) => {
+    const requestMessage = {
+      type: MESSAGE_TYPE.EXTERNAL_SIGN_SEND,
+      target: BACKGROUND_PORT,
+      token: 'signSendToken',
+      requestId: 'signSendRequestId',
+      data: {
+        tx: signSendRequest.tx,
+        target: 'LOCK_HASH',
+        config: { index: 0, length: -1 },
+        meta: signSendRequest.meta,
       },
-      '*',
-    );
-    return new Promise((resolve) => {
-      window.addEventListener(
-        'message',
-        (e) => {
-          const message = e.data;
-          const isMessageValid = message.type && message.port === WEB_PAGE;
-          if (isMessageValid) {
-            resolve(message);
-          }
-        },
-        false,
-      );
-    });
-  },
-  sign: (message: RPCMessage.SignRequest) => {
-    if (message.type !== MESSAGE_TYPE.EXTERNAL_SIGN) return;
-    window.postMessage(message, '*');
-  },
-  send: (message: RPCMessage.SignRequest) => {
-    if (message.type !== MESSAGE_TYPE.EXTERNAL_SEND) return;
-    window.postMessage(message, '*');
-  },
-  signSend: (message: RPCMessage.SignRequest) => {
-    if (message.type !== MESSAGE_TYPE.EXTERNAL_SIGN_SEND) return;
-    window.postMessage(message, '*');
+    };
+
+    return promisedMessageHandler(requestMessage);
   },
 };
 
-console.log('ckb is injected successfully');
+console.log('Injected ckb successfully');
