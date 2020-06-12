97b301e23c3cfa0070687800e1f521fdb7a42d13
Harry Liu <2411mail@gmail.com>
2020-06-12 20:55:22 +0800

    add capacity to api

--- src/background/messageHandlers/getAddressInfo.ts
+++ src/background/messageHandlers/getAddressInfo.ts
@@ -1,15 +1,21 @@
 import * as browser from 'webextension-polyfill';
 import { MESSAGE_TYPE } from '@utils/constants';
 import { WEB_PAGE } from '@utils/message/constants';
+import { getAddressInfo } from '@utils/apis';
 
 export default async (port) => {
   const { currentWallet } = await browser.storage.local.get(['currentWallet']);
+  let capacity = '0';
+  if (currentWallet) {
+    const addressInfo = await getAddressInfo(currentWallet?.lock);
+    capacity = addressInfo.capacity;
+  }
   port.postMessage({
     type: MESSAGE_TYPE.EXTERNAL_GET_ADDRESS_INFO,
     requestId: 1,
     success: true,
-    message: 'get address info successfully',
+    message: currentWallet ? 'get address info successfully' : 'do not have wallet info',
     target: WEB_PAGE,
-    data: currentWallet,
+    data: currentWallet ? { ...currentWallet, capacity } : undefined,
   });
 };
