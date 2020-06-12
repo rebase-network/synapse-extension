8d3e5958ee362a1040a49e22320d19804da9e419
Harry Liu <2411mail@gmail.com>
2020-06-12 19:04:03 +0800

    refactor: improve logic

--- src/background/messageHandlers/index.ts
+++ src/background/messageHandlers/index.ts
@@ -1,14 +1,15 @@
 import * as browser from 'webextension-polyfill';
+import * as _ from 'lodash';
 import { MESSAGE_TYPE } from '@utils/constants';
 import send from './send';
 import getAddressInfoHandler from './getAddressInfo';
 
 const handler = async (message, port) => {
-  if (message.type === MESSAGE_TYPE.EXTERNAL_SIGN_SEND && !message.success) {
+  if (message.type === MESSAGE_TYPE.EXTERNAL_SIGN_SEND) {
     send(port, message);
   }
 
-  if (message.type === MESSAGE_TYPE.EXTERNAL_GET_ADDRESS_INFO && !message.success) {
+  if (message.type === MESSAGE_TYPE.EXTERNAL_GET_ADDRESS_INFO) {
     // TODO: capacity
     getAddressInfoHandler(port);
   }
@@ -17,6 +18,8 @@ const handler = async (message, port) => {
 export default () => {
   browser.runtime.onConnect.addListener((port) => {
     port.onMessage.addListener(async (message: any) => {
+      const messageHandled = _.has(message, 'success');
+      if (messageHandled) return;
       handler(message, port);
     });
   });
