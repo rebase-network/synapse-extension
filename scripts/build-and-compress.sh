e6d167a1c0c75cc8d923d498766939dfa4560d5e
Harry Liu <2411mail@gmail.com>
2020-06-13 13:13:20 +0800

    update script

--- scripts/build-and-compress.sh
+++ scripts/build-and-compress.sh
@@ -6,10 +6,9 @@ yarn build
 
 mv dist synapse-extension
 
-# 打包压缩
 zip -q -r synapse-extension.zip synapse-extension
 
 rm -rf dist
 
 # 文件的校验和
-shasum synapse-extension.zip
+shasum -a 256 synapse-extension.zip | tee synapse-extension.asc
