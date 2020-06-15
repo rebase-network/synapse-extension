84157efe733f2d041a56df471a0debd698138bdf
liushooter <shooter.hero@gmail.com>
2020-06-16 00:04:10 +0800

    update getUnspentCells api

--- src/utils/apis.ts
+++ src/utils/apis.ts
@@ -7,10 +7,12 @@ export const getAddressInfo = async (lockHash: string): Promise<{ capacity: stri
   return result.data;
 };
 
-export const getUnspentCells = async (lockHash: string, isEmpty: string = 'true') => {
+export const getUnspentCells = async (lockHash: string) => {
+  // cell/getUnspentCells/:lockHash?isEmpty=true
   const result = await Axios.get(
-    `${configService.CACHE_LAYER_ENDPOINT}/cell/getUnspentCells/${lockHash}/${isEmpty}`,
+    `${configService.CACHE_LAYER_ENDPOINT}/cell/getUnspentCells/${lockHash}?isEmpty=true`,
   );
+
   return result.data;
 };
 
