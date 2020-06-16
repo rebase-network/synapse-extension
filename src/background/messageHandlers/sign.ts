63968b5dfa96487b1f5c7151121b35bceb8a9b5a
Harry Liu <2411mail@gmail.com>
2020-06-16 23:25:57 +0800

    remove console log

--- src/background/messageHandlers/sign.ts
+++ src/background/messageHandlers/sign.ts
@@ -11,16 +11,12 @@ export default async (port, message) => {
 
   let notifWindow = await browser.tabs.query({ windowType: 'popup' });
 
-  console.log('Before notifications 111111111111 ');
   await browser.notifications.create({
     type: 'basic',
     iconUrl: 'logo-32.png',
     title: 'Tabs reloaded',
     message: 'Your tabs have been reloaded',
   });
-  console.log('After notifications 222222222222222 ');
-
-  console.log('Before windows.create 333333333333 ');
 
   if (_.isEmpty(notifWindow)) {
     notifWindow = await browser.windows.create({
@@ -39,29 +35,21 @@ export default async (port, message) => {
   };
   notifyExtension(msg);
 
-  console.log('After windows.create 4444444444: ', notifWindow);
-
   try {
     const intervalId = setInterval(async () => {
-      console.log('setInterval, notifWindow: ', notifWindow);
       const tabs = await browser.tabs.query({ windowId: notifWindow.id });
-      console.log(tabs);
       // status: [loading, complete]
       if (tabs?.[0]?.status === 'complete') {
-        console.log(' get tabs complete, will clear aaaaaaaaa ');
-
         clearInterval(intervalId);
         browser.runtime.sendMessage({
           type: MESSAGE_TYPE.GOTO_SIGN_PAGE,
           data: message.data,
         });
-        console.log('After sendMessage 55555555555555 ');
       }
     }, 2000);
   } catch (error) {
     console.log('query tabs error: ', error);
   }
-  console.log('Sign method end 6666666666 ');
 
   // chrome.windows.create(
   //   {
