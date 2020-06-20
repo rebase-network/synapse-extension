7e52378d6d948b362de55efe133bb95a3e0f2a38
syuukawa <syuukawa@hotmail.com>
2020-06-20 22:11:05 +0800

    used the Axios interceptors instead of data.data

--- src/utils/apis.ts
+++ src/utils/apis.ts
@@ -1,10 +1,22 @@
 import Axios from 'axios';
 import { configService } from '../config';
 
+// Add a response interceptor
+Axios.interceptors.response.use(
+  (response) => {
+    // Do something with response data
+    return response.data;
+  },
+  (error) => {
+    // Do something with response error
+    return Promise.reject(error);
+  },
+);
+
 export const getAddressInfo = async (lockHash: string): Promise<{ capacity: string }> => {
   // call api
   const result = await Axios.get(`${configService.CACHE_LAYER_ENDPOINT}/address/${lockHash}`);
-  return result.data.data;
+  return result.data;
 };
 
 export const getUnspentCells = async (lockHash: string) => {
@@ -13,13 +25,13 @@ export const getUnspentCells = async (lockHash: string) => {
     `${configService.CACHE_LAYER_ENDPOINT}/cell/getUnspentCells/${lockHash}?isEmpty=true`,
   );
 
-  return result.data.data;
+  return result.data;
 };
 
 export const getTxHistories = async (scriptObj): Promise<any> => {
   const url = `${configService.CACHE_LAYER_ENDPOINT}/cell/getTxHistories`;
   const result = await Axios.post(url, scriptObj);
-  return result.data.data;
+  return result.data;
 };
 
 export default {
