c0de42f72d9e0db852fcbc09eb6c83732ac452ee
Harry Liu <2411mail@gmail.com>
2020-06-23 12:58:35 +0800

    remove unused code

--- src/keyper/keyperwallet.ts
+++ src/keyper/keyperwallet.ts
@@ -13,13 +13,10 @@ const { Container } = require('@keyper/container/lib');
 const { Secp256k1LockScript } = require('@keyper/container/lib/locks/secp256k1');
 const { AnyPayLockScript } = require('@keyper/container/lib/locks/anyone-can-pay');
 const Keccak256LockScript = require('./locks/keccak256');
-const storage = require('./storage');
 
 // eslint-disable-next-line import/order
 const EC = require('elliptic').ec;
 
-let seed;
-let keys;
 let container;
 const addRules = []; // Mod by River
 
@@ -39,7 +36,6 @@ const init = () => {
           return a;
         },
         async sign(context, message) {
-          // const key = keys[context.publicKey];
           const key = await getKeystoreFromWallets(context.publicKey);
           if (!key) {
             throw new Error(`no key for address: ${context.address}`);
@@ -77,73 +73,13 @@ const init = () => {
   container.addLockScript(
     new AnyPayLockScript(ADDRESS_TYPE_CODEHASH.AnyPay, 'type', getDepFromLockType('AnyPay')),
   );
-  // keys = {};
-  // reloadKeys();
 };
-
-const reloadKeys = () => {
-  if (storage.keyperStorage().get('keys')) {
-    const innerKeys = storage.keyperStorage().get('keys');
-    innerKeys.forEach((key) => {
-      container.addPublicKey({
-        payload: `0x${key.publicKey}`,
-        algorithm: SignatureAlgorithm.secp256k1,
-      });
-      keys[`0x${key.publicKey}`] = key;
-    });
-  }
-};
-
-// const reloadCacheRuls = async () => {
-//   if (storage.keyperStorage().get("keys")) {
-//     const innerKeys = storage.keyperStorage().get("keys");
-//     innerKeys.forEach(key => {
-//       const scripts = container.getScripsByPublicKey({
-//         payload: `0x${key.publicKey}`,
-//         algorithm: SignatureAlgorithm.secp256k1,
-//       });
-//       scripts.forEach(async (script) => {
-//         // await global.cache.addRule({
-//         //   name: "LockHash",
-//         //   data: scriptToHash(script),
-//         // });
-//         const addRule = {
-//           name: "LockHash",
-//           data: scriptToHash(script),
-//         }
-//         addRules.push(addRule);
-//       });
-//     });
-//   }
-// };
 
 const generateKeystore = async (privateKey, password) => {
   const privateKeyBuffer = Buffer.from(privateKey, 'hex');
   const ks = await Keystore.encrypt(privateKeyBuffer, password);
   return ks;
 };
-
-// ks -> keystore
-// const saveKeystore = (ks, publicKey) => {
-//   keys = {};
-//   ks.publicKey = publicKey;
-//   if (!storage.keyperStorage().get('keys')) {
-//     storage.keyperStorage().set('keys', JSON.stringify(ks));
-//   } else {
-//     const keys = storage.keyperStorage().get('keys');
-//     keys.push(JSON.stringify(ks));
-//     storage.keyperStorage().set('keys', keys);
-//   }
-
-//   // keys[`0x${publicKey}`] = key;
-//   keys[`0x${publicKey}`] = ks;
-
-//   return keys;
-// };
-
-// const saveKeystoreToWallet = (ks, wallet) => {
-//     wallet.Keystore = ks;
-// }
 
 const setUpContainer = (publicKey) => {
   container.addPublicKey({
@@ -157,10 +93,6 @@ const setUpContainer = (publicKey) => {
   });
 
   scripts.forEach(async (script) => {
-    // await global.cache.addRule({
-    //   name: "LockHash",
-    //   data: scriptToHash(script),
-    // });
     const addRule = {
       name: 'LockHash',
       data: scriptToHash(script),
@@ -173,47 +105,8 @@ const generateByPrivateKey = async (privateKey, password) => {
   const ec = new EC('secp256k1');
   const key = ec.keyFromPrivate(privateKey);
   const publicKey = Buffer.from(key.getPublic().encodeCompressed()).toString('hex');
-  // const privateKeyBuffer = Buffer.from(privateKey, "hex")
-  // const ks = Keystore.encrypt(privateKeyBuffer, password);
   const ks = generateKeystore(privateKey, password);
-  // ks.publicKey = publicKey;
 
-  // if (!storage.keyperStorage().get("keys")) {
-  //   storage.keyperStorage().set("keys", JSON.stringify(ks));
-  // } else {
-  //   const keys = storage.keyperStorage().get("keys");
-  //   keys.push(JSON.stringify(ks));
-  //   storage.keyperStorage().set("keys", keys);
-  // }
-  //   saveKeystore(ks, publicKey);
-
-  // keys[`0x${publicKey}`] = key;
-  // keys[`0x${publicKey}`] = ks;
-
-  // //container在init中进行初始化的操作;根据PublicKey生成Address
-  // container.addPublicKey({
-  //   payload: `0x${publicKey}`,
-  //   algorithm: SignatureAlgorithm.secp256k1,
-  // });
-
-  // const scripts = container.getScripsByPublicKey({
-  //   payload: `0x${publicKey}`,
-  //   algorithm: SignatureAlgorithm.secp256k1,
-  // });
-
-  // scripts.forEach(async (script) => {
-  //   // await global.cache.addRule({
-  //   //   name: "LockHash",
-  //   //   data: scriptToHash(script),
-  //   // });
-  //   const addRule = {
-  //     name: "LockHash",
-  //     data: scriptToHash(script),
-  //   }
-  //   // console.log(" === script === ",script)
-  //   // console.log(" === LockHash === ",scriptToHash(script));
-  //   addRules.push(addRule);
-  // });
   setUpContainer(publicKey);
 
   return ks;
@@ -261,17 +154,11 @@ const getAllLockHashesAndMeta = async () => {
 
 module.exports = {
   init,
-  // createPassword,
-  // getSeed,
-  // unlock,
-  // exists,
   generateByPrivateKey,
   accounts,
   signTx,
   getAllLockHashesAndMeta,
-  // reloadCacheRuls,
   generateKeystore,
-  //  saveKeystore,
   setUpContainer,
   container,
 };
