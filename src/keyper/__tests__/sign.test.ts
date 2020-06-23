b1c7645f9fc03e18be6fa37343eb2435e49b04c0
Harry Liu <2411mail@gmail.com>
2020-06-23 13:24:53 +0800

    update test

--- src/keyper/__tests__/sign.test.ts
+++ src/keyper/__tests__/sign.test.ts
@@ -3,7 +3,7 @@ import { aliceAddresses } from '@src/test/fixture/address';
 import { txHash } from './fixture';
 
 describe('Sign tx with secp256k1 lock', () => {
-  it('sign with config, index 3, length 2', () => {
+  it('sign a tx hash', () => {
     const targetSignMsg =
       '0xd1e172abccec16973df781ec6a4a19b0aa9930be7e8ef9b6b7b43d0bda95b9ac4a271ed96e28164c0d15136be5b0d47a1c087aac76b7ce5b8f36caa095f6794a00';
 
