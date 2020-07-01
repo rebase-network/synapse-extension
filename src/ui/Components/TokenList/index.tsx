ebb40b0d7aa7145271405ebaef245ba14bb7cf70
Harry Liu <2411mail@gmail.com>
2020-07-01 17:53:48 +0800

    get data from api then handle and send to child component

--- src/ui/Components/TokenList/index.tsx
+++ src/ui/Components/TokenList/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import { getUDTsByLockHash } from '@utils/apis';
+import { aggregateUDT } from '@utils/token';
 import TokenListComponent from './component';
 
 const useStyles = makeStyles({
@@ -16,21 +18,34 @@ interface AppProps {
   udtsCapacity: any[];
 }
 
-export default (props: AppProps) => {
+export default () => {
   const [tokenList, setTokenList] = React.useState([]);
   const [loading, setLoading] = React.useState(true);
   const classes = useStyles();
-  const { udtsCapacity } = props;
-  console.log('props: ', props);
   React.useEffect(() => {
-    browser.storage.local.get('tokenList').then((result) => {
+    browser.storage.local.get('udts').then((result) => {
       setLoading(false);
       if (!result.tokenList) return;
       setTokenList(result.tokenList);
     });
+
+    async function getUDTs(): Promise<void> {
+      const { currentWallet } = await browser.storage.local.get('currentWallet');
+      if (!currentWallet) return;
+      const { lock: lockHash } = currentWallet;
+      const udtsWithCapacity = await getUDTsByLockHash({
+        lockHash,
+      });
+      console.log('udtsWithCapacity: ', udtsWithCapacity);
+      setTokenList(udtsWithCapacity.udts);
+    }
+    getUDTs();
   }, []);
 
   if (loading) return <div className={classes.loading}>Loading Token List...</div>;
+  if (tokenList.length === 0) return null;
+  const udtsCapacity = aggregateUDT(tokenList);
+  console.log('udtsCapacity: ', udtsCapacity);
 
   const resultElem = <TokenListComponent udtsCapacity={udtsCapacity} />;
 
