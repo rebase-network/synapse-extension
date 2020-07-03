ac1d36aa458206318c1bd96206abb971ee1e4994
Harry Liu <2411mail@gmail.com>
2020-07-03 11:42:45 +0800

    add eslint-plugin-jest-dom

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,17 @@
 // Refer to: https://github.com/iamturns/eslint-config-airbnb-typescript#i-use-eslint-config-airbnb-with-react-support
 
 module.exports = {
-  plugins: ['prettier', 'testing-library'],
+  plugins: [
+    'prettier',
+    'testing-library',
+    'jest-dom',
+  ],
   extends: [
     'airbnb-typescript',
     'plugin:prettier/recommended',
     'plugin:testing-library/recommended',
-    "plugin:testing-library/react"
+    "plugin:testing-library/react",
+    'plugin:jest-dom/recommended',
   ],
   parserOptions: {
     project: './tsconfig.json',
