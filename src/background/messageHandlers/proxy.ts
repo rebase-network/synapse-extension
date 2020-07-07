8205728ac044892ee203ff3f7393f50c891e906a
Harry Liu <2411mail@gmail.com>
2020-07-07 16:43:34 +0800

    should close popup after finish sending message

--- src/background/messageHandlers/proxy.ts
+++ src/background/messageHandlers/proxy.ts
@@ -1,11 +1,14 @@
+import { getLastWindowId } from '@common/popup';
 
 export const sendToWebPage = (message) => {
-  function sendToContentScript(tabs) {
+  const sendToContentScript = async (tabs) => {
     // send back reponse to web page
     if (tabs[0]?.id) {
       browser.tabs.sendMessage(tabs[0]?.id, message);
+      const windowId = await getLastWindowId();
+      browser.windows.remove(windowId);
     }
-  }
+  };
   browser.tabs
     .query({ currentWindow: false, active: true })
     .then(sendToContentScript, console.error);
