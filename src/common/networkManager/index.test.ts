3a2955a52589e89497ea7509e0c1f40386a4b9a9
Harry Liu <2411mail@gmail.com>
2020-07-09 22:42:22 +0800

    update test

--- src/common/networkManager/index.test.ts
+++ src/common/networkManager/index.test.ts
@@ -2,30 +2,26 @@ import NetworkManager from './index';
 import { networks } from './constants';
 
 describe('network manager', () => {
-  beforeEach(() => {
-    NetworkManager.initNetworks();
+  beforeEach(async () => {
+    await NetworkManager.initNetworks();
   });
-
-  afterEach(() => {
-    browser.storage.local.clear();
+  afterEach(async () => {
+    await NetworkManager.reset();
   });
 
   it('should return initial networks', async () => {
-    console.log('---------', networks);
     const result = await NetworkManager.getNetworkList();
     expect(result).toHaveLength(networks.length);
   });
 
   it('should able to set current network', async () => {
     const result = await NetworkManager.getCurrentNetwork();
-    console.log('---------', networks);
     expect(result).toBe(networks[0]);
     await NetworkManager.setCurrentNetwork(networks[0].name);
     expect(await NetworkManager.getCurrentNetwork()).toBe(networks[0]);
   });
 
   it('should able to create a network', async () => {
-    console.log('---------', networks);
     const result = await NetworkManager.createNetwork({
       name: 'my network',
       nodeURL: 'testtest.com',
@@ -36,13 +32,11 @@ describe('network manager', () => {
   });
 
   it('should able to get network info', async () => {
-    console.log('---------', networks);
     const result = await NetworkManager.getNetwork('Aggron Testnet');
     expect(result).toBe(networks[0]);
   });
 
   it('should able to remove a network', async () => {
-    console.log('---------', networks);
     const result = await NetworkManager.removeNetwork('Aggron Testnet');
     expect(result).toHaveLength(networks.length);
   });
