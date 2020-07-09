ffe750ed5fdc6e6949d41955480ca6a501b516e2
Harry Liu <2411mail@gmail.com>
2020-07-09 17:32:07 +0800

    refactor config

--- src/config.ts
+++ src/config.ts
@@ -1,37 +1,13 @@
-class ConfigService {
-  env: any;
+export default {
+  get CKB_RPC_ENDPOINT(): string {
+    return process.env.CKB_RPC_ENDPOINT;
+  },
 
-  constructor() {
-    this.env = process.env;
-  }
+  get CACHE_LAYER_ENDPOINT(): string {
+    return process.env.CACHE_LAYER_ENDPOINT;
+  },
 
-  private getValue(key: string, throwOnMissing = true): string {
-    const value = this.env[key];
-    if (!value && throwOnMissing) {
-      throw new Error(`config error - missing env.${key}`);
-    }
-
-    return value;
-  }
-
-  public get(key: string): string {
-    return this.getValue(key, true);
-  }
-
-  public ensureValues(keys: string[]) {
-    keys.forEach((k) => this.getValue(k, true));
-    return this;
-  }
-
-  public isProduction() {
-    const mode = this.getValue('MODE', false);
-    return mode !== 'DEV';
-  }
-}
-
-const configService = new ConfigService().ensureValues([
-  'CKB_RPC_ENDPOINT',
-  'CACHE_LAYER_ENDPOINT',
-]);
-
-export default configService;
+  get isProduction(): boolean {
+    return process.env.MODE !== 'DEV';
+  },
+};
