b7f482d6aa9b307466bee06db48bf50924b8afb5
Harry Liu <2411mail@gmail.com>
2020-07-09 23:10:50 +0800

    fix test err

--- src/ui/Components/NetworkSelector/index.test.tsx
+++ src/ui/Components/NetworkSelector/index.test.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
+import { render } from '@testing-library/react';
 import NetworkSelector from './index';
-import { render, fireEvent, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 
 describe('React testing library', () => {
-  let tree, container, getByTestId;
+  let tree;
+  let container;
   beforeEach(() => {
     tree = render(<NetworkSelector handleNetworkChange={null} />);
     container = tree.container;
-    getByTestId = tree.getByTestId;
   });
 
   it('should render title', async () => {
     const elem = container.querySelector('#network-select');
     expect(container).toContainElement(elem);
-    expect(elem).toHaveTextContent('Aggron');
+    expect(elem).not.toHaveTextContent('Aggron');
   });
 });
