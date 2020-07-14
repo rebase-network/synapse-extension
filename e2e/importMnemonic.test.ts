36d25cba93dc78458403455fd5b07a0df06bc0f0
Harry Liu <2411mail@gmail.com>
2020-07-14 23:42:38 +0800

    show extension in puppeteer browser

--- e2e/importMnemonic.test.ts
+++ e2e/importMnemonic.test.ts
@@ -1,15 +1,16 @@
 import puppeteer from 'puppeteer';
-// const path = require('path');
 
-// // Path to the actual extension we want to be testing
-// const pathToExtension = require('path').join(path.join(__dirname, '..', 'dist'));
+const path = require('path');
 
-// // Tell puppeteer we want to load the web extension
-// const puppeteerArgs = [
-//   `--disable-extensions-except=${pathToExtension}`,
-//   `--load-extension=${pathToExtension}`,
-//   '--show-component-extension-options',
-// ];
+// Path to the actual extension we want to be testing
+const pathToExtension = require('path').join(path.join(__dirname, '..', 'dist'));
+
+// Tell puppeteer we want to load the web extension
+const puppeteerArgs = [
+  `--disable-extensions-except=${pathToExtension}`,
+  `--load-extension=${pathToExtension}`,
+  '--show-component-extension-options',
+];
 
 jest.setTimeout(30000);
 
@@ -20,11 +21,11 @@ describe('Google', () => {
   beforeAll(async () => {
     // browser = await puppeteer.launch();
     browser = await puppeteer.launch({
-      // headless: false,
-      headless: true,
+      headless: false,
+      // headless: true,
       slowMo: 250,
       devtools: true,
-      // args: puppeteerArgs,
+      args: puppeteerArgs,
     });
 
     //   // Creates a new tab
