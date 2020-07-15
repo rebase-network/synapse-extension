6eb77e8d733c6b0682c421c0e3ddb9fafc0c82bb
Harry Liu <2411mail@gmail.com>
2020-07-15 17:31:04 +0800

    remove unused file and test suite

--- src/test/wallet.test.ts
+++ src/test/wallet.test.ts
@@ -1,6 +1,4 @@
-import * as CkbUtils from '@nervosnetwork/ckb-sdk-utils';
-import { generateMnemonic, ExtendedPrivateKey } from '../wallet/key';
-import Keystore from './keystore';
+import { generateMnemonic } from '../wallet/key';
 import Keychain from '../wallet/keychain';
 import { mnemonicToSeedSync } from '../wallet/mnemonic';
 
@@ -10,14 +8,6 @@ const fixture = {
   seed:
     '878386efb78845b3355bd15ea4d39ef97d179cb712b77d5c12b6be415fffeffe5f377ba02bf3f8544ab800b955e51fbff09828f682052a20faa6addbbddfb096',
 };
-
-const fixture2 = {
-  privateKey: 'e8f32e723decf4051aefac8e2c93c9c5b214313817cdb01a1494b917c8436b35',
-  publicKey: '0339a36013301597daef41fbe593a02cc513d0b55527ec2df1050e2e8ff49c85c2',
-  chainCode: '873dff81c02f525623fd1fe5167eac3a55a049de3d314bb42ee227ffed37d508',
-};
-
-const mnenonics = 'excuse speak boring lunar consider sea behave next fog arrow black sweet';
 
 describe('wallet', () => {
   const longSeed = Buffer.from(
@@ -48,72 +38,5 @@ describe('wallet', () => {
     expect(master.index).toEqual(0);
     expect(master.depth).toEqual(0);
     expect(master.parentFingerprint).toEqual(0);
-  });
-
-  it('decrypt keystore json file', () => {
-    const passwd = '111111';
-
-    const seed = mnemonicToSeedSync(mnenonics);
-    const masterKeychain = Keychain.fromSeed(seed);
-
-    const extendedPrivateKey = new ExtendedPrivateKey(
-      masterKeychain.privateKey.toString('hex'),
-      masterKeychain.chainCode.toString('hex'),
-    );
-
-    const keystore = Keystore.create(extendedPrivateKey, passwd);
-    const jsonObj = keystore.toJson();
-
-    const newkeystore = Keystore.fromJson(JSON.stringify(jsonObj));
-
-    const masterPrivateKey = newkeystore.extendedPrivateKey(passwd);
-
-    const newMasterKeychain = new Keychain(
-      Buffer.from(masterPrivateKey.privateKey, 'hex'),
-      Buffer.from(masterPrivateKey.chainCode, 'hex'),
-    );
-
-    const hdPrivateKey = `0x${newMasterKeychain
-      .derivePath("m/44'/309'/0'/0")
-      .deriveChild(0, false)
-      .privateKey.toString('hex')}`;
-
-    const defaultAddressOptions: CkbUtils.AddressOptions = {
-      prefix: CkbUtils.AddressPrefix.Testnet,
-      type: CkbUtils.AddressType.HashIdx,
-      codeHashOrCodeHashIndex: '0x00',
-    };
-
-    const hdtestnetAddr = CkbUtils.privateKeyToAddress(hdPrivateKey, defaultAddressOptions);
-
-    expect(hdtestnetAddr).toBe('ckt1qyqt9ed4emcxyfed77ed0dp7kcm3mxsn97ls38jxjw');
-  });
-});
-
-describe('load and check password', () => {
-  const password = 'hello~!23';
-  const keystore = Keystore.create(
-    new ExtendedPrivateKey(fixture2.privateKey, fixture2.chainCode),
-    password,
-  );
-
-  it('checks wrong password', () => {
-    expect(keystore.checkPassword(`oops${password}`)).toBe(false);
-  });
-
-  it('checks correct password', () => {
-    expect(keystore.checkPassword(password)).toBe(true);
-  });
-
-  it('decrypts', () => {
-    expect(keystore.decrypt(password)).toEqual(
-      new ExtendedPrivateKey(fixture2.privateKey, fixture2.chainCode).serialize(),
-    );
-  });
-
-  it('loads private key', () => {
-    const extendedPrivateKey = keystore.extendedPrivateKey(password);
-    expect(extendedPrivateKey.privateKey).toEqual(fixture2.privateKey);
-    expect(extendedPrivateKey.chainCode).toEqual(fixture2.chainCode);
   });
 });
