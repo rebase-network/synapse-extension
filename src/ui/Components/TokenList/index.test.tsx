89a638caff1250d0c7ffcc00110740b0fb8a1ff7
Harry Liu <2411mail@gmail.com>
2020-07-17 14:17:38 +0800

    fix test issue

--- src/ui/Components/TokenList/index.test.tsx
+++ src/ui/Components/TokenList/index.test.tsx
@@ -1,15 +1,32 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { render, screen } from '@testing-library/react';
 import { IntlProvider } from 'react-intl';
 import en from '@common/locales/en';
 import { udtsCapacity, udtsMeta } from '@utils/tests/fixtures/token';
 import Component from './component';
 
+jest.mock('react-router-dom', () => {
+  // Require the original module to not be mocked...
+  const originalModule = jest.requireActual('react-router-dom');
+
+  return {
+    __esModule: true,
+    ...originalModule,
+    // add your noops here
+    useParams: jest.fn(),
+    useHistory: jest.fn(),
+    Link: 'a',
+  };
+});
+
 describe('token list', () => {
   beforeEach(() => {
     render(
       <IntlProvider locale="en" messages={en}>
-        <Component udtsCapacity={udtsCapacity} udtsMeta={udtsMeta} />
+        <Router>
+          <Component udtsCapacity={udtsCapacity} udtsMeta={udtsMeta} />
+        </Router>
       </IntlProvider>,
     );
   });
