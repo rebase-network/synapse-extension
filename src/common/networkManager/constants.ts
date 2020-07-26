898c7bffb3cd9fcaa0c02a3eaa6fa31d173751c7
liushooter <shooter.hero@gmail.com>
2020-07-26 13:21:38 +0800

    network const

--- src/common/networkManager/constants.ts
+++ src/common/networkManager/constants.ts
@@ -1,24 +1,25 @@
 import configService from '@src/config';
-
 export const networks = [
   {
-    name: 'Aggron Testnet',
+    title: 'Lina Mainnet',
+    networkType: 'mainnet',
+    prefix: 'ckb',
+    nodeURL: configService.CKB_RPC_ENDPOINT_MAINNET,
+    cacheURL: configService.CACHE_LAYER_ENDPOINT_MAINNET,
+  },
+  {
+    title: 'Aggron Testnet',
+    networkType: 'testnet',
+    prefix: 'ckt',
     nodeURL: configService.CKB_RPC_ENDPOINT,
     cacheURL: configService.CACHE_LAYER_ENDPOINT,
-    prefix: 'ckt',
   },
-  //   {
-  //     name: 'Local',
-  //     nodeURL: 'http://127.0.0.1:8114',
-  //     cacheURL: 'http://127.0.0.1:3000',
-  //     prefix: 'ckt',
-  //   },
-  //   {
-  //     name: 'Mainnet',
-  //     nodeURL: configService.CKB_RPC_ENDPOINT_MAINNET,
-  //     cacheURL: configService.CACHE_LAYER_ENDPOINT_MAINNET,
-  //     prefix: 'ckb',
-  //   },
+  {
+    title: 'local',
+    networkType: 'testnet',
+    prefix: 'ckt',
+    nodeURL: 'http://127.0.0.1:8114',
+    cacheURL: 'http://127.0.0.1:3000',
+  },
 ];
-
 export default networks;
