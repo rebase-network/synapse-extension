7aff8042cb221119fd99f614d2da28280059847a
Harry Liu <2411mail@gmail.com>
2020-07-31 00:53:36 +0800

    remove default value

--- src/keyper/locks/commonLockScript.ts
+++ src/keyper/locks/commonLockScript.ts
@@ -1,6 +1,6 @@
 import { ScriptHashType, CellDep, SignatureAlgorithm, SignProvider, DepType } from '@keyper/specs';
-// https://basarat.gitbook.io/typescript/type-system/mixins
 
+// https://basarat.gitbook.io/typescript/type-system/mixins
 // Needed for all mixins
 type Constructor<T = {}> = new (...args: any[]) => T;
 
@@ -15,7 +15,7 @@ export default function CommonLockScript<TBase extends Constructor>(Base: TBase)
 
     private hashType: ScriptHashType = 'type';
 
-    private provider: SignProvider = null;
+    private provider: SignProvider;
 
     private algo: SignatureAlgorithm = SignatureAlgorithm.secp256k1;
 
