7ddf2dd1c6a3ccebd7627545a4006756eda4b284
Harry Liu <2411mail@gmail.com>
2020-07-31 00:49:27 +0800

    move constants to a file

---
+++ src/keyper/locksInfo.ts
@@ -0,0 +1,48 @@
+import { ScriptHashType } from '@keyper/specs';
+
+const testnet = 'testnet';
+const mainnet = 'mainnet';
+export const NETWORKS = [testnet, mainnet];
+
+export default {
+  [testnet]: {
+    secp256k1: {
+      codeHash: '0x9bd7e06f3ecf4be0f2fcd2188b23f1b9fcc88e5d4b65a8637b17723bbda3cce8',
+      hashType: 'type' as ScriptHashType,
+      txHash: '0x84dcb061adebff4ef93d57c975ba9058a9be939d79ea12ee68003f6492448890',
+      depType: 'depGroup',
+    },
+    keccak256: {
+      codeHash: '0x0',
+      hashType: 'code' as ScriptHashType,
+      txHash: '0x0',
+      depType: 'depGroup',
+    },
+    anypay: {
+      codeHash: '0x86a1c6987a4acbe1a887cca4c9dd2ac9fcb07405bbeda51b861b18bbf7492c4b',
+      hashType: 'type' as ScriptHashType,
+      txHash: '0x4f32b3e39bd1b6350d326fdfafdfe05e5221865c3098ae323096f0bfc69e0a8c',
+      depType: 'depGroup',
+    },
+  },
+  [mainnet]: {
+    secp256k1: {
+      codeHash: '0x9bd7e06f3ecf4be0f2fcd2188b23f1b9fcc88e5d4b65a8637b17723bbda3cce8',
+      hashType: 'type' as ScriptHashType,
+      txHash: '0x71a7ba8fc96349fea0ed3a5c47992e3b4084b031a42264a018e0072e8172e46c',
+      depType: 'depGroup',
+    },
+    keccak256: {
+      codeHash: '0x0',
+      hashType: 'code' as ScriptHashType,
+      txHash: '0x0',
+      depType: 'code',
+    },
+    anypay: {
+      codeHash: '0x86a1c6987a4acbe1a887cca4c9dd2ac9fcb07405bbeda51b861b18bbf7492c4b',
+      hashType: 'type' as ScriptHashType,
+      txHash: '0x4f32b3e39bd1b6350d326fdfafdfe05e5221865c3098ae323096f0bfc69e0a8c',
+      depType: 'depGroup',
+    },
+  },
+};
