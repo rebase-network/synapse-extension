a6ec3ddf60bdd681b460e2354ec5672f814fb677
Harry Liu <2411mail@gmail.com>
2020-07-31 02:05:27 +0800

    refactoring factory code, rename network to name

--- src/keyper/setupKeyper.ts
+++ src/keyper/setupKeyper.ts
@@ -9,62 +9,64 @@ import { getKeystoreFromWallets } from '../wallet/addKeyperWallet';
 import * as Keystore from '../wallet/passwordEncryptor';
 import { sign as signBySecp256k1 } from './sign';
 
-const containerFactory = () => {
-  const container = new Container([
-    {
-      algorithm: SignatureAlgorithm.secp256k1,
-      provider: {
-        async sign(context, message) {
-          const key = await getKeystoreFromWallets(context.publicKey);
-          if (!key) {
-            throw new Error(`no key for address: ${context.address}`);
-          }
-          const privateKeyBuffer = await Keystore.decrypt(key, context.password);
-          const Uint8ArrayPk = new Uint8Array(privateKeyBuffer.data);
-          const privateKey = ckbUtils.bytesToHex(Uint8ArrayPk);
+const containerFactory = {
+  createContainer: () => {
+    const container = new Container([
+      {
+        algorithm: SignatureAlgorithm.secp256k1,
+        provider: {
+          async sign(context, message) {
+            const key = await getKeystoreFromWallets(context.publicKey);
+            if (!key) {
+              throw new Error(`no key for address: ${context.address}`);
+            }
+            const privateKeyBuffer = await Keystore.decrypt(key, context.password);
+            const Uint8ArrayPk = new Uint8Array(privateKeyBuffer.data);
+            const privateKey = ckbUtils.bytesToHex(Uint8ArrayPk);
 
-          const signature = signBySecp256k1(privateKey, message);
-          return signature;
+            const signature = signBySecp256k1(privateKey, message);
+            return signature;
+          },
         },
       },
-    },
-  ]);
-  return container;
+    ]);
+    return container;
+  },
 };
 
 export default () => {
   const manager = ContainerManager.getInstance();
-  NETWORKS.forEach((network) => {
+  NETWORKS.forEach((name) => {
     manager.addContainer({
-      network,
-      container: containerFactory(),
+      name,
+      container: containerFactory.createContainer(),
     });
   });
   const containers = manager.getAllContainers();
   // add lock script for all containers
-  Object.keys(containers).forEach((network) => {
-    const container = containers[network];
+  Object.keys(containers).forEach((name) => {
+    const container = containers[name];
     // add lock script
     container.addLockScript(
       new Secp256k1LockScript(
-        LOCKS_INFO[network].secp256k1.codeHash,
-        LOCKS_INFO[network].secp256k1.txHash,
-        LOCKS_INFO[network].secp256k1.hashType,
+        LOCKS_INFO[name].secp256k1.codeHash,
+        LOCKS_INFO[name].secp256k1.txHash,
+        LOCKS_INFO[name].secp256k1.hashType,
         new Secp256k1LockScriptOriginal(),
       ),
     );
     container.addLockScript(
       new Keccak256LockScript(
-        LOCKS_INFO[network].keccak256.codeHash,
-        LOCKS_INFO[network].keccak256.txHash,
-        LOCKS_INFO[network].keccak256.hashType,
+        LOCKS_INFO[name].keccak256.codeHash,
+        LOCKS_INFO[name].keccak256.txHash,
+        LOCKS_INFO[name].keccak256.hashType,
       ),
     );
     container.addLockScript(
       new AnypayLockScript(
-        LOCKS_INFO[network].anypay.codeHash,
-        LOCKS_INFO[network].anypay.txHash,
-        LOCKS_INFO[network].anypay.hashType,
+        LOCKS_INFO[name].anypay.codeHash,
+        LOCKS_INFO[name].anypay.txHash,
+        LOCKS_INFO[name].anypay.hashType,
       ),
     );
   });
