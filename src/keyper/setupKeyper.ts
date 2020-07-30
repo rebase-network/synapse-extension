d8d6e3e49136c786db0cac245181155692c9ce9d
Harry Liu <2411mail@gmail.com>
2020-07-31 00:50:03 +0800

    refactoring: use real lock script info

--- src/keyper/setupKeyper.ts
+++ src/keyper/setupKeyper.ts
@@ -1,9 +1,9 @@
 import { Container } from '@keyper/container';
 import { SignatureAlgorithm } from '@keyper/specs';
+import { Secp256k1LockScript as Secp256k1LockScriptOriginal } from '@keyper/container/lib/locks/secp256k1';
 import ContainerManager from './containerManager';
 import { Keccak256LockScript, AnypayLockScript, Secp256k1LockScript } from './locks';
-
-const networks = ['testnet', 'mainnet'];
+import LOCKS_INFO, { NETWORKS } from './locksInfo';
 
 const containerFactory = () => {
   const container = new Container([
@@ -21,18 +21,38 @@ const containerFactory = () => {
 
 export default () => {
   const manager = ContainerManager.getInstance();
-  networks.forEach((network) => {
+  NETWORKS.forEach((network) => {
     manager.addContainer({
       network,
       container: containerFactory(),
     });
   });
   const containers = manager.getAllContainers();
+  // add lock script for all containers
   Object.keys(containers).forEach((network) => {
     const container = containers[network];
     // add lock script
-    container.addLockScript(new Secp256k1LockScript('1', '2'));
-    container.addLockScript(new Keccak256LockScript('1', '2'));
-    container.addLockScript(new AnypayLockScript('1', '2'));
+    container.addLockScript(
+      new Secp256k1LockScript(
+        LOCKS_INFO[network].secp256k1.codeHash,
+        LOCKS_INFO[network].secp256k1.txHash,
+        LOCKS_INFO[network].secp256k1.hashType,
+        new Secp256k1LockScriptOriginal(),
+      ),
+    );
+    container.addLockScript(
+      new Keccak256LockScript(
+        LOCKS_INFO[network].keccak256.codeHash,
+        LOCKS_INFO[network].keccak256.txHash,
+        LOCKS_INFO[network].keccak256.hashType,
+      ),
+    );
+    container.addLockScript(
+      new AnypayLockScript(
+        LOCKS_INFO[network].anypay.codeHash,
+        LOCKS_INFO[network].anypay.txHash,
+        LOCKS_INFO[network].anypay.hashType,
+      ),
+    );
   });
 };
