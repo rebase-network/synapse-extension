53ba44c0ff4aad066adc0ae915048915b7256582
Harry Liu <2411mail@gmail.com>
2020-07-31 11:43:13 +0800

    refactor: combine the 2 files which have similar features and cycle dependencies

--- src/keyper/keyperwallet.ts
+++ src/keyper/keyperwallet.ts
@@ -1,12 +1,23 @@
 import { SignatureAlgorithm } from '@keyper/specs/lib';
 import { scriptToHash } from '@nervosnetwork/ckb-sdk-utils/lib';
 import { scriptToAddress } from '@keyper/specs/lib/address';
+import * as ckbUtils from '@nervosnetwork/ckb-sdk-utils';
+import _ from 'lodash';
 import * as Keystore from '../wallet/passwordEncryptor';
 import ContainerManager from './containerManager';
 import init from './setupKeyper';
 
+import { KEYSTORE_TYPE } from '../utils/constants';
+import Address, { AddressPrefix } from './address';
+// FIXME
+import KeyperWallet from './keyperwallet';
+
 // eslint-disable-next-line import/order
 const EC = require('elliptic').ec;
+
+let wallets = [];
+let currentWallet = {};
+const addressesList = [];
 
 const container = ContainerManager.getInstance().getContainer('testnet');
 const addRules = [];
@@ -81,13 +92,128 @@ const getAllLockHashesAndMeta = async () => {
   return container.getAllLockHashesAndMeta();
 };
 
-export default {
+// privateKey No '0x'
+async function addKeyperWallet(privateKey, password, entropyKeystore, rootKeystore, networkPrefix) {
+  init();
+
+  const keystore = await generateKeystore(privateKey, password);
+  // prefix '0x'
+  const publicKey = ckbUtils.privateKeyToPublicKey(`0x${privateKey}`);
+  // params publicKey No '0x'
+  setUpContainer(publicKey.substr(2));
+
+  // Keyper accounts
+  const accounts = await accounts(networkPrefix);
+
+  // eslint-disable-next-line @typescript-eslint/no-use-before-define
+  await saveWallets(privateKey, keystore, accounts, entropyKeystore, rootKeystore);
+
+  return accounts;
+}
+
+async function getWalletsInStorage() {
+  const walletsObj = await browser.storage.local.get('wallets');
+
+  if (Array.isArray(walletsObj.wallets)) {
+    wallets = walletsObj.wallets;
+  }
+  return wallets;
+}
+
+// 3- Type
+// privateKey Not contain '0x'prefix
+async function saveWallets(
+  privateKey,
+  keystore,
+  accounts,
+  entropyKeystore,
+  rootKeystore,
+  prefix = AddressPrefix.Testnet,
+) {
+  const addressObj = Address.fromPrivateKey(privateKey, prefix);
+  const blake160 = addressObj.getBlake160(); // publicKeyHash
+  const publicKey = ckbUtils.privateKeyToPublicKey(`0x${privateKey}`);
+
+  wallets = await getWalletsInStorage();
+
+  if (_.isEmpty(wallets)) {
+    wallets = [];
+  }
+
+  const walletCommon = {
+    publicKey,
+    blake160,
+    entropyKeystore,
+    rootKeystore,
+    keystore,
+    keystoreType: KEYSTORE_TYPE.PRIVATEKEY_TO_KEYSTORE,
+  };
+
+  wallets.push(walletCommon);
+
+  const addressesObj = {
+    publicKey,
+    addresses: accounts,
+  };
+
+  addressesList.push(addressesObj);
+
+  const currentAddress = {
+    publicKey,
+    script: accounts[0].script,
+    address: accounts[0].address,
+    type: accounts[0].type,
+    lock: accounts[0].lock,
+  };
+
+  currentWallet = currentAddress;
+}
+
+function getWallets() {
+  return wallets;
+}
+
+function getAddressesList() {
+  return addressesList;
+}
+
+function getCurrentWallet() {
+  return currentWallet;
+}
+
+// eslint-disable-next-line no-shadow
+function findKeystoreInWallets(wallets, publicKey) {
+  function findKeystore(wallet) {
+    return wallet.publicKey === publicKey;
+  }
+  const wallet = wallets.find(findKeystore);
+  return wallet.keystore;
+}
+
+async function getKeystoreFromWallets(publicKey) {
+  let nPublicKey = publicKey;
+  if (!publicKey.startsWith('0x')) {
+    nPublicKey = `0x${publicKey}`;
+  }
+  wallets = await getWalletsInStorage();
+  const ks = findKeystoreInWallets(wallets, nPublicKey);
+  // keys[nPublicKey]
+  return ks;
+}
+
+export {
   init,
   generateByPrivateKey,
-  accounts,
   signTx,
   getAllLockHashesAndMeta,
   generateKeystore,
   setUpContainer,
   container,
+  addKeyperWallet,
+  getWalletsInStorage,
+  saveWallets,
+  getWallets,
+  getAddressesList,
+  getCurrentWallet,
+  getKeystoreFromWallets,
 };
