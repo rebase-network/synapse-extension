6489a879b01c1f304751e8b6c792dad0d4ab8c2f
Harry Liu <2411mail@gmail.com>
2020-08-01 19:04:45 +0800

    refactor: move files around

--- src/keyper/locks/tests/anypay.test.ts
+++ src/keyper/locks/tests/anypay.test.ts
@@ -1,19 +1,19 @@
-import Keccak256LockScript from '../locks/keccak256';
+import AnypayLockScript from '../anypay';
 
-describe('keccak lockscript', () => {
-  const codeHash = '0x9bd7e06f3ecf4be0f2fcd2188b23f1b9fcc88e5d4b65a8637b17723bbda3cce8';
-  const txHash = '0x25635bf587adacf95c9ad302113648f89ecddc2acfe1ea358ea99f715219c4c5';
+describe('anypay lockscript', () => {
+  const codeHash = '0x86a1c6987a4acbe1a887cca4c9dd2ac9fcb07405bbeda51b861b18bbf7492c4b';
+  const txHash = '0x4f32b3e39bd1b6350d326fdfafdfe05e5221865c3098ae323096f0bfc69e0a8c';
   const index = '0x0';
   const depType = 'code';
 
   it('basic', () => {
-    const lock = new Keccak256LockScript(codeHash, txHash);
+    const lock = new AnypayLockScript(codeHash, txHash);
     const script = lock.script(
       '0x020ea44dd70b0116ab44ade483609973adf5ce900d7365d988bc5f352b68abe50b',
     );
     expect(script).toEqual(
       expect.objectContaining({
-        args: '0xf7f62b9be3a4aab818dc4e706b7d4fa29738d91b',
+        args: '0xedcda9513fa030ce4308e29245a22c022d0443bb',
         codeHash,
         hashType: 'type',
       }),
@@ -21,7 +21,7 @@ describe('keccak lockscript', () => {
   });
 
   it('deps', () => {
-    const lock = new Keccak256LockScript(codeHash, txHash);
+    const lock = new AnypayLockScript(codeHash, txHash);
     const deps = lock.deps();
     expect(deps).toEqual([
       {
