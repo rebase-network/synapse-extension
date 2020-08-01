4cd22d4b5e0729de79358ce53cf6dc9f93fa0c9f
Harry Liu <2411mail@gmail.com>
2020-08-01 19:08:25 +0800

    remove files after moving

--- src/keyper/tests/fixtures/signInfo.ts
+++
@@ -1,8 +0,0 @@
-import { aliceAddresses } from '@src/test/fixture/address';
-
-export default {
-  message: '0x32d4365e154c064dac98ff272941be0fc716765a284c931d9a07a1fafec83dc5',
-  signedMessage:
-    '0x02bd47677087bd6056f2dd3737249d6c475acbe425dc7d34c97dbab434086d9d4db14676ebd29a8c392ae61291f3ee89d1a00f8792e6b51cc881d8bf5280ac7200',
-  privateKey: aliceAddresses.privateKey,
-};
