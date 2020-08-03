ad3986efa7517804c0e1d6b456925b24e456b9fd
Harry Liu <2411mail@gmail.com>
2020-08-03 20:03:10 +0800

    add index field to locks info

--- src/utils/constants/locksInfo.ts
+++ src/utils/constants/locksInfo.ts
@@ -11,6 +11,7 @@ export default {
       hashType: 'type' as ScriptHashType,
       txHash: '0xf8de3bb47d055cdf460d93a2a6e1b05f7432f9777c8c474abf4eec1d4aee5d37',
       depType: 'depGroup',
+      index: '0x0',
     },
     keccak256: {
       codeHash: '0xa5b896894539829f5e7c5902f0027511f94c70fa2406d509e7c6d1df76b06f08',
@@ -18,12 +19,14 @@ export default {
       // TODO
       txHash: '0x0',
       depType: 'depGroup',
+      index: '0x0',
     },
     anypay: {
       codeHash: '0x86a1c6987a4acbe1a887cca4c9dd2ac9fcb07405bbeda51b861b18bbf7492c4b',
       hashType: 'type' as ScriptHashType,
       txHash: '0x4f32b3e39bd1b6350d326fdfafdfe05e5221865c3098ae323096f0bfc69e0a8c',
       depType: 'depGroup',
+      index: '0x0',
     },
   },
   [mainnet]: {
@@ -32,6 +35,7 @@ export default {
       hashType: 'type' as ScriptHashType,
       txHash: '0x71a7ba8fc96349fea0ed3a5c47992e3b4084b031a42264a018e0072e8172e46c',
       depType: 'depGroup',
+      index: '0x0',
     },
     keccak256: {
       codeHash: '0xa5b896894539829f5e7c5902f0027511f94c70fa2406d509e7c6d1df76b06f08',
@@ -39,12 +43,14 @@ export default {
       // TODO
       txHash: '0x0',
       depType: 'code',
+      index: '0x0',
     },
     anypay: {
       codeHash: '0x86a1c6987a4acbe1a887cca4c9dd2ac9fcb07405bbeda51b861b18bbf7492c4b',
       hashType: 'type' as ScriptHashType,
       txHash: '0x4f32b3e39bd1b6350d326fdfafdfe05e5221865c3098ae323096f0bfc69e0a8c',
       depType: 'depGroup',
+      index: '0x0',
     },
   },
 };
