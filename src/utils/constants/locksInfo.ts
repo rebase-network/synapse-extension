bc43edc116edb29684361f8d40dc6666f7008579
Harry Liu <2411mail@gmail.com>
2020-08-03 21:31:04 +0800

    update locks info

--- src/utils/constants/locksInfo.ts
+++ src/utils/constants/locksInfo.ts
@@ -4,6 +4,7 @@ import { NETWORK_TYPES } from '@utils/constants/networks';
 const { testnet, mainnet } = NETWORK_TYPES;
 export const NETWORKS = [testnet, mainnet];
 
+// https://github.com/nervosnetwork/rfcs/blob/master/rfcs/0024-ckb-system-script-list/0024-ckb-system-script-list.md
 export default {
   [testnet]: {
     secp256k1: {
@@ -16,7 +17,7 @@ export default {
     keccak256: {
       codeHash: '0xa5b896894539829f5e7c5902f0027511f94c70fa2406d509e7c6d1df76b06f08',
       hashType: 'code' as ScriptHashType,
-      // TODO
+      // TODO: not deployed yet
       txHash: '0x0',
       depType: 'depGroup',
       index: '0x0',
@@ -40,7 +41,7 @@ export default {
     keccak256: {
       codeHash: '0xa5b896894539829f5e7c5902f0027511f94c70fa2406d509e7c6d1df76b06f08',
       hashType: 'code' as ScriptHashType,
-      // TODO
+      // TODO: not deployed yet
       txHash: '0x0',
       depType: 'code',
       index: '0x0',
@@ -48,7 +49,8 @@ export default {
     anypay: {
       codeHash: '0x86a1c6987a4acbe1a887cca4c9dd2ac9fcb07405bbeda51b861b18bbf7492c4b',
       hashType: 'type' as ScriptHashType,
-      txHash: '0x4f32b3e39bd1b6350d326fdfafdfe05e5221865c3098ae323096f0bfc69e0a8c',
+      // TODO: not deployed yet
+      txHash: '0x0',
       depType: 'depGroup',
       index: '0x0',
     },
