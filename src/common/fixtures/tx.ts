b910347db2aeca1843aa9c9bfef970e6a5322a5f
Harry Liu <2411mail@gmail.com>
2020-08-03 20:02:40 +0800

    update fixture

--- src/common/fixtures/tx.ts
+++ src/common/fixtures/tx.ts
@@ -229,7 +229,7 @@ const signedWitnesses = [
     outputType: '',
   },
   // The value will be different depends on the private key to sign. The signed witness position/index should unchange (3)
-  '0x550000001000000055000000550000004100000002bd47677087bd6056f2dd3737249d6c475acbe425dc7d34c97dbab434086d9d4db14676ebd29a8c392ae61291f3ee89d1a00f8792e6b51cc881d8bf5280ac7200',
+  signedMessage,
   {
     inputType: '',
     lock: '',
@@ -242,7 +242,7 @@ const signedWitnessesSerialized = [
   '0x10000000100000001000000010000000',
   '0x10000000100000001000000010000000',
   // The value will be different depends on the private key to sign. The signed witness position/index should unchange (3)
-  '0x550000001000000055000000550000004100000002bd47677087bd6056f2dd3737249d6c475acbe425dc7d34c97dbab434086d9d4db14676ebd29a8c392ae61291f3ee89d1a00f8792e6b51cc881d8bf5280ac7200',
+  signedMessage,
   '0x10000000100000001000000010000000',
 ];
 
