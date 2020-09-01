03f4b23104bd954dd9ebf0c0125fc0952499019b
syuukawa <syuukawa@hotmail.com>
2020-09-01 15:53:56 +0800

    CKB show add other(Full) but not SUDT

--- src/ui/Components/TokenList/index.tsx
+++ src/ui/Components/TokenList/index.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
 import { makeStyles } from '@material-ui/core/styles';
-import { ListItem, ListItemText } from '@material-ui/core';
+import { ListItem, ListItemText, Tooltip } from '@material-ui/core';
 import { getUDTsByLockHash } from '@utils/apis';
 import { aggregateUDT } from '@utils/token';
 import { shannonToCKBFormatter } from '@utils/formatters';
 import { TypesInfo } from '@src/utils/constants/typesInfo';
 import NetworkManager from '@src/common/networkManager';
 import _ from 'lodash';
+import { HelpOutline } from '@material-ui/icons';
 import TokenListComponent from './component';
 
 const useStyles = makeStyles({
@@ -24,6 +25,20 @@ const useStyles = makeStyles({
   ckb: {
     'border-bottom': '1px solid #ccc',
   },
+  ckbFull: {
+    display: 'flex',
+    'align-items': 'center',
+  },
+  ckbFullText: {
+    'margin-right': 8,
+  },
+  helpIcon: {
+    'font-size': '1.2rem',
+    color: 'gray',
+  },
+  displayName: {
+    color: 'inherit',
+  },
 });
 
 interface AppProps {
@@ -35,6 +50,7 @@ export default (props: AppProps) => {
   const [udtsMeta, setUdtsMeta] = React.useState([]);
   const [loading, setLoading] = React.useState(true);
   const [emptyCapacity, setEmptyCapacity] = React.useState('0');
+  const [othersCapacity, setOthersCapacity] = React.useState('0');
   const { explorerUrl } = props;
 
   const classes = useStyles();
@@ -56,6 +72,7 @@ export default (props: AppProps) => {
       const { networkType } = await NetworkManager.getCurrentNetwork();
       const typeInfo = TypesInfo[networkType.toLowerCase()];
       const sudtCodeHash = typeInfo.simpleudt.codeHash;
+
       // Just Show Simple UDT
       const udtsAllWithCapacity = AllWithCapacity.udts;
       const udts = _.filter(udtsAllWithCapacity, function find(item) {
@@ -64,6 +81,16 @@ export default (props: AppProps) => {
 
       setTokenList(udts);
       setEmptyCapacity(shannonToCKBFormatter(AllWithCapacity.capacity));
+
+      // expect SUDT
+      const others = _.filter(udtsAllWithCapacity, function find(item) {
+        return item.typeHash !== null && item.type.codeHash !== sudtCodeHash;
+      });
+      const othersCellsCapacity = others.reduce(
+        (prev, next) => BigInt(prev) + BigInt(next.capacity),
+        0,
+      );
+      setOthersCapacity(othersCellsCapacity);
     }
     getUDTs();
   }, []);
@@ -88,6 +115,19 @@ export default (props: AppProps) => {
     <TokenListComponent udtsCapacity={udtsCapacity} udtsMeta={udtsMeta} explorerUrl={explorerUrl} />
   );
 
+  const displayName = (
+    <Tooltip
+      title={<FormattedMessage id="CKB with data inside, it's not UDT" />}
+      arrow
+      placement="top"
+    >
+      <div className={classes.ckbFull}>
+        <span className={classes.ckbFullText}>Other (Full)</span>
+        <HelpOutline className={classes.helpIcon} />
+      </div>
+    </Tooltip>
+  );
+
   return (
     <div className={classes.root}>
       <ListItem disableGutters className={classes.ckb}>
@@ -95,6 +135,15 @@ export default (props: AppProps) => {
         <ListItemText primary={`${emptyCapacity} CKB`} className={classes.token} />
       </ListItem>
       {resultElem}
+      {/* other Cells except SUDT */}
+      <ListItem disableGutters className={classes.ckb}>
+        <ListItemText primary={displayName} />
+        <ListItemText
+          primary="0"
+          secondary={`${shannonToCKBFormatter(othersCapacity.toString())} CKB`}
+          className={classes.token}
+        />
+      </ListItem>
     </div>
   );
 };
