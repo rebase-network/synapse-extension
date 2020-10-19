ca1b68bf6f6325d20d48d31dbe4e54ff38c1a413
lastbiter <dont@send.to>
2020-10-19 22:15:57 +0800

    add address field

--- src/background/messageHandlers/getAddressInfo.ts
+++ src/background/messageHandlers/getAddressInfo.ts
@@ -1,6 +1,8 @@
 import { MESSAGE_TYPE } from '@utils/constants';
 import { WEB_PAGE } from '@utils/message/constants';
 import { getAddressInfo } from '@utils/apis';
+import { scriptToAddress } from '@keyper/specs/lib/address';
+import NetworkManager from '@common/networkManager';
 
 export default async (port) => {
   const { currentWallet } = await browser.storage.local.get(['currentWallet']);
@@ -9,12 +11,17 @@ export default async (port) => {
     const addressInfo = await getAddressInfo(currentWallet?.lock);
     capacity = addressInfo.capacity;
   }
+  const currentNetwork = await NetworkManager.getCurrentNetwork();
+  const address = scriptToAddress(currentWallet.script, {
+    networkPrefix: currentNetwork.prefix,
+    short: true,
+  });
   port.postMessage({
     type: MESSAGE_TYPE.EXTERNAL_GET_ADDRESS_INFO,
     requestId: 'getAddressInfo',
     success: true,
     message: currentWallet ? 'get address info successfully' : 'do not have wallet info',
     target: WEB_PAGE,
-    data: currentWallet ? { ...currentWallet, capacity } : undefined,
+    data: currentWallet ? { ...currentWallet, capacity, address } : undefined,
   });
 };
