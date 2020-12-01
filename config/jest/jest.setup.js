e05d1dae1c0135ab618bdfe2010022fd34a0bd2c
Harry <2411mail@gmail.com>
2020-12-01 15:40:20 +0800

    update test case

--- config/jest/jest.setup.js
+++ config/jest/jest.setup.js
@@ -12,16 +12,16 @@ Object.assign(global.crypto, {
   ...crypto,
   subtle: {
     encrypt: () => {
-      console.error('ERROR: It is a fake function. encrypt is not implemented in jsdom');
+      console.warn('ERROR: It is a fake function. encrypt is not implemented in jsdom');
     },
     decrypt: () => {
-      console.error('ERROR: It is a fake function. decrypt is not implemented in jsdom');
+      console.warn('ERROR: It is a fake function. decrypt is not implemented in jsdom');
     },
     importKey: () => {
-      console.error('ERROR: It is a fake function. importKey is not implemented in jsdom');
+      console.warn('ERROR: It is a fake function. importKey is not implemented in jsdom');
     },
     deriveKey: () => {
-      console.error('ERROR: It is a fake function. deriveKey is not implemented in jsdom');
+      console.warn('ERROR: It is a fake function. deriveKey is not implemented in jsdom');
     },
   },
 });
--- src/keyper/signProviders/secp256k1.test.ts
+++ src/keyper/signProviders/secp256k1.test.ts
@@ -1,5 +1,6 @@
 import { wallets as walletsSample } from '@src/wallet/fixtures/wallets';
-import { getWalletsInStorage } from './secp256k1';
+import { aliceAddresses, aliceWallet } from '@src/test/fixture/address';
+import signProvider, { getWalletsInStorage } from './secp256k1';
 
 describe('secp256k1 sign provider', () => {
   it('should be able to set wallets to storage', async () => {
@@ -7,11 +8,45 @@ describe('secp256k1 sign provider', () => {
 
     expect(wallets).toBeUndefined();
 
+    const currWallets = await getWalletsInStorage();
+
+    expect(currWallets).toEqual([]);
+
     await browser.storage.local.set({
       wallets: walletsSample,
     });
     const newWallets = await getWalletsInStorage();
 
     expect(newWallets).toEqual(walletsSample);
+  });
+
+  it('should sign correctly', async () => {
+    const context = {
+      privateKey: aliceAddresses.privateKey,
+      publicKey: aliceAddresses.publicKey,
+      address: aliceAddresses.secp256k1.address,
+      password: 'aaa',
+    };
+    const result = await signProvider.sign(context, '0x123');
+    const expected =
+      '0xaccccd1be484021413e886e78b24d6abd20b3d3f9c9a37252b709ad2114aac3a5c63439109349d14d9aec7d8d300476155d9c82375e9625083da9a58f6234fe501';
+    expect(result).toEqual(expected);
+  });
+
+  it('should sign correctly with context of not having private key', async () => {
+    await browser.storage.local.set({
+      wallets: [aliceWallet],
+    });
+
+    const context = {
+      publicKey: aliceAddresses.publicKey,
+      address: aliceAddresses.secp256k1.address,
+      password: 'aaaaaa',
+    };
+    try {
+      await signProvider.sign(context, '0x123');
+    } catch (error) {
+      // no action
+    }
   });
 });
