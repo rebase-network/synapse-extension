5528d57279960df06706f18ae663d9ee8825eca1
Harry <2411mail@gmail.com>
2020-12-04 00:29:02 +0800

    add test case

--- src/background/wallet/transaction/tests/anypay.test.ts
+++ src/background/wallet/transaction/tests/anypay.test.ts
@@ -5,7 +5,8 @@ import { getUnspentCells } from '@src/common/utils/apis';
 import configService from '@src/config';
 import { bobAddresses, aliceAddresses } from '@src/tests/fixture/address';
 import { anypayDep } from '@src/tests/fixture/deps';
-import { createRawTx } from '../txGenerator';
+import { createRawTx, createAnyPayRawTx } from '../txGenerator';
+import unspentCells from './fixtures/cells';
 
 jest.mock('@common/utils/apis');
 
@@ -27,7 +28,6 @@ describe('Transaction test', () => {
     const params = {
       capacity: toAmount,
     };
-    const unspentCells = await getUnspentCells(lockHash, params);
 
     function getTotalCapity(total, cell) {
       return BigInt(total) + BigInt(cell.capacity);
@@ -44,5 +44,82 @@ describe('Transaction test', () => {
     const signedTx = ckb.signTransaction(privateKey)(signObj.tx, []);
     const realTxHash = await ckb.rpc.sendTransaction(signedTx);
     expect(realTxHash).toEqual('0x123');
+  });
+
+  it('createAnyPayRawTx test anyonepay', async () => {
+    const { privateKey } = bobAddresses;
+    const fromAddress = bobAddresses.anyPay.address;
+    const toAddress = aliceAddresses.anyPay.address;
+
+    const lock = addressToScript(fromAddress);
+    const toLock = addressToScript(toAddress);
+    const toAmount = new BN(11100000000);
+    const fee = new BN(100000000);
+
+    const deps = [anypayDep];
+    const lockHash = bobAddresses.anyPay.lock;
+    const params = {
+      capacity: toAmount,
+    };
+
+    function getTotalCapity(total, cell) {
+      return BigInt(total) + BigInt(cell.capacity);
+    }
+    const totalCapity = unspentCells.reduce(getTotalCapity, 0);
+
+    const cells = {
+      cells: unspentCells,
+      total: new BN(totalCapity),
+    };
+
+    const anyPayRawTx = createAnyPayRawTx(
+      toAmount,
+      toLock,
+      cells,
+      lock,
+      deps,
+      fee,
+      cells,
+      totalCapity,
+    );
+    const expected = {
+      fee: '05f5e100',
+      target: '0x6af8c2199802665ea2a8ed85b087b1ac26b47b332dfe7322c6b34e38f1a2f129',
+      tx: {
+        cellDeps: [
+          {
+            depType: 'depGroup',
+            outPoint: {
+              index: '0x0',
+              txHash: '0x4f32b3e39bd1b6350d326fdfafdfe05e5221865c3098ae323096f0bfc69e0a8c',
+            },
+          },
+        ],
+        headerDeps: [],
+        inputs: [
+          {
+            previousOutput: {
+              index: '0x1',
+              txHash: '0x37063697f29aaa185cb971ce658872eebdee419f3389ab746674c65c9b8a96b6',
+            },
+            since: '0x0',
+          },
+        ],
+        outputs: [
+          {
+            capacity: '0x5e3ff5d00',
+            lock: {
+              args: '0x5c0eaae525d4fc2e04b162c84f286dfd51af4002',
+              codeHash: '0x86a1c6987a4acbe1a887cca4c9dd2ac9fcb07405bbeda51b861b18bbf7492c4b',
+              hashType: 'type',
+            },
+          },
+        ],
+        outputsData: ['0x'],
+        version: '0x0',
+        witnesses: ['0x', { inputType: '', lock: '', outputType: '' }],
+      },
+    };
+    expect(anyPayRawTx.tx).toEqual(expected.tx);
   });
 });
---
+++ src/background/wallet/transaction/tests/fixtures/cells.ts
@@ -0,0 +1,28 @@
+const unspentCells = [
+  {
+    blockHash: '0x49e71fde5904f67005b72712b49ddf52a97a7abe10bd5183c31f8b3a5d936121',
+    lock: {
+      codeHash: '0x9bd7e06f3ecf4be0f2fcd2188b23f1b9fcc88e5d4b65a8637b17723bbda3cce8',
+      hashType: 'type',
+      args: '0x8eb4d75815e2207bd29a0b3651523e5a48e8fd34',
+    },
+    lockHash: '0x111823010653d32d36b18c9a257fe13158ca012e22b9b82f0640be187f10904b',
+    outPoint: {
+      txHash: '0x37063697f29aaa185cb971ce658872eebdee419f3389ab746674c65c9b8a96b6',
+      index: '0x1',
+    },
+    outputData: '0x449b2c00000000000000000000000000',
+    outputDataLen: '0x10',
+    capacity: '0x34e62ce00',
+    type: {
+      codeHash: '0x48dbf59b4c7ee1547238021b4869bceedf4eea6b43772e5d66ef8865b6ae7212',
+      hashType: 'data',
+      args: '0x6e842ebb7d7fca88495c5f2edb05070198f6f8c798d7b8f1a48226f8f060c693',
+    },
+    typeHash: '0x1c5f32c5efb08ac256bd9413c30eef7420ae54f30ff11075967b89570326c295',
+    dataHash: '0x85b991d8b373274224b781301765cddd366959acf294f58f56bb56ed5b695516',
+    status: 'live',
+  },
+];
+
+export default unspentCells;
