521841d72989d69ea3ddf43c29d6ae4c4226b679
Harry <2411mail@gmail.com>
2020-12-03 16:17:15 +0800

    add test case

--- src/background/keyper/locks/tests/keccak256.test.ts
+++ src/background/keyper/locks/tests/keccak256.test.ts
@@ -1,3 +1,7 @@
+import { aliceAddresses, aliceWalletPwd } from '@src/tests/fixture/address';
+import { rawTx } from '@common/fixtures/tx';
+import signProvider from '@background/keyper/signProviders/secp256k1';
+import LOCKS_INFO from '@common/utils/constants/locksInfo';
 import Keccak256LockScript from '../keccak256';
 
 describe('keccak lockscript', () => {
@@ -35,5 +39,27 @@ describe('keccak lockscript', () => {
         depType,
       },
     ]);
+  });
+
+  it('sign', async () => {
+    const {
+      privateKey,
+      publicKey,
+      secp256k1: { address },
+    } = aliceAddresses;
+    const lock = new Keccak256LockScript(
+      LOCKS_INFO.testnet.keccak256.codeHash,
+      LOCKS_INFO.testnet.keccak256.txHash,
+      LOCKS_INFO.testnet.keccak256.hashType,
+    );
+
+    lock.setProvider(signProvider);
+
+    const context = { privateKey, publicKey, address, aliceWalletPwd };
+
+    const result = await lock.sign(context, rawTx);
+    const expected =
+      '0x5500000010000000550000005500000041000000b1998b1641c80d67acabbff4b776b215d10a0426289e073683c0f16d13bee5307ac6d447c41ac1af8984dc805011bee6ee673c025c1131e85ff6bf8c1cb023be00';
+    expect(result.witnesses[0]).toEqual(expected);
   });
 });
