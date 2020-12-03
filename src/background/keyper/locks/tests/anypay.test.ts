990621ad214b8c39d891e6ddfb53d40badd4817d
Harry <2411mail@gmail.com>
2020-12-03 16:14:23 +0800

    add test case

--- src/background/keyper/locks/tests/anypay.test.ts
+++ src/background/keyper/locks/tests/anypay.test.ts
@@ -1,3 +1,7 @@
+import { aliceAddresses, aliceWalletPwd } from '@src/tests/fixture/address';
+import { rawTx } from '@common/fixtures/tx';
+import signProvider from '@background/keyper/signProviders/secp256k1';
+import LOCKS_INFO from '@common/utils/constants/locksInfo';
 import AnypayLockScript from '../anypay';
 
 describe('anypay lockscript', () => {
@@ -35,5 +39,27 @@ describe('anypay lockscript', () => {
         depType,
       },
     ]);
+  });
+
+  it('sign', async () => {
+    const {
+      privateKey,
+      publicKey,
+      secp256k1: { address },
+    } = aliceAddresses;
+    const lock = new AnypayLockScript(
+      LOCKS_INFO.testnet.anypay.codeHash,
+      LOCKS_INFO.testnet.anypay.txHash,
+      LOCKS_INFO.testnet.anypay.hashType,
+    );
+
+    lock.setProvider(signProvider);
+
+    const context = { privateKey, publicKey, address, aliceWalletPwd };
+
+    const result = await lock.sign(context, rawTx);
+    const expected =
+      '0x5500000010000000550000005500000041000000d1e172abccec16973df781ec6a4a19b0aa9930be7e8ef9b6b7b43d0bda95b9ac4a271ed96e28164c0d15136be5b0d47a1c087aac76b7ce5b8f36caa095f6794a00';
+    expect(result.witnesses[0]).toEqual(expected);
   });
 });
