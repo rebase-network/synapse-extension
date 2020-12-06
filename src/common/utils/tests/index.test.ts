c9475c0778799bc60bc0395aa10b42fcb587fff4
Harry <2411mail@gmail.com>
2020-12-06 17:29:56 +0800

    add test

--- src/common/utils/tests/index.test.ts
+++ src/common/utils/tests/index.test.ts
@@ -1,6 +1,21 @@
-import { parseSUDT } from '../index';
+import { textToHex, textToBytesLength, parseSUDT } from '../index';
 
 describe('utils', () => {
+  it('textToHex', () => {
+    const result = textToHex('abc');
+    expect(result).toEqual('0x616263');
+  });
+
+  it('textToHex', () => {
+    const result = textToHex('0x123');
+    expect(result).toEqual('0x123');
+  });
+
+  it('textToBytesLength', () => {
+    const result = textToBytesLength('abc');
+    expect(result).toEqual(3);
+  });
+
   it('should be able to parse sUDT amount', () => {
     const outputData = '10270000000000000000000000000000';
     const udtAmount = parseSUDT(outputData);
