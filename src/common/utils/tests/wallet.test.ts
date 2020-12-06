e613d33c7bbb534b0cb51e0926e03274b14c7152
Harry <2411mail@gmail.com>
2020-12-06 17:41:11 +0800

    add test

---
+++ src/common/utils/tests/wallet.test.ts
@@ -0,0 +1,35 @@
+import { aliceAddresses, aliceWallet } from '@src/tests/fixture/address';
+import {
+  findInWalletsByPublicKey,
+  findInAddressesListByPublicKey,
+  showAddressHelper,
+} from '../wallet';
+import addressesList from './fixtures/addressesList';
+
+const { publicKey } = aliceAddresses;
+
+describe('wallet utils', () => {
+  beforeAll(async () => {
+    await browser.storage.local.set({
+      publicKeys: [publicKey],
+      wallets: [aliceWallet],
+    });
+  });
+  it('findInWalletsByPublicKey', () => {
+    const result = findInWalletsByPublicKey(publicKey, [aliceWallet]);
+    expect(result).not.toBeNull();
+  });
+  it('findInAddressesListByPublicKey', () => {
+    const result = findInAddressesListByPublicKey(publicKey, addressesList);
+    expect(result).toEqual(undefined);
+  });
+  it('showAddressHelper', () => {
+    const script = {
+      args: '0x60ed0599d4a5c67fd25277243ac12b9f91517b61',
+      codeHash: '0x9bd7e06f3ecf4be0f2fcd2188b23f1b9fcc88e5d4b65a8637b17723bbda3cce8',
+      hashType: 'type',
+    };
+    const result = showAddressHelper('ckb', script);
+    expect(result).toEqual('ckb1qyqxpmg9n822t3nl6ff8wfp6cy4ely230dssrs7k9z');
+  });
+});
