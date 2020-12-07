7350c8e39de8a39f4a5e2e12de270588a94eab6a
Harry <2411mail@gmail.com>
2020-12-07 18:27:04 +0800

    add test for upload file component

---
+++ src/ui/pages/ImportPrivateKey/UploadFile.test.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import '@testing-library/jest-dom/extend-expect';
+import { IntlProvider } from 'react-intl';
+import en from '@common/locales/en';
+import App from './UploadFile';
+
+describe('Upload file', () => {
+  beforeEach(() => {
+    render(
+      <IntlProvider locale="en" messages={en}>
+        <App />
+      </IntlProvider>,
+    );
+  });
+  it('upload file', async () => {
+    const uploadBtn = await screen.findByRole('button', { name: 'Upload Keystore JSON File' });
+    expect(uploadBtn).toBeInTheDocument();
+
+    const file = new File(['hello'], 'hello.png', { type: 'image/png' });
+    const input = screen.getByLabelText('Upload Keystore JSON File') as HTMLInputElement;
+
+    userEvent.upload(input, file);
+
+    expect(input.files[0]).toStrictEqual(file);
+    expect(input.files.item(0)).toStrictEqual(file);
+    expect(input.files).toHaveLength(1);
+  });
+});
