709bc02249cafababac735b2ef77a581429df048
Harry <2411mail@gmail.com>
2020-12-14 14:43:55 +0800

    add tests for setting page

--- src/ui/pages/Setting/index.test.tsx
+++ src/ui/pages/Setting/index.test.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { act, render, screen, waitFor, waitForElement } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { BrowserRouter as Router, useHistory } from 'react-router-dom';
 import { IntlProvider } from 'react-intl';
-import chrome from 'sinon-chrome';
-
-import App from '@ui/pages/Setting/index';
+import App from '@ui/pages/Setting';
 import en from '@common/locales/en';
+import { MESSAGE_TYPE } from '@src/common/utils/constants';
 
 const mockFunc = jest.fn();
 
@@ -26,37 +26,99 @@ jest.mock('react-router-dom', () => {
 });
 
 describe('setting page', () => {
-  beforeAll(() => {
-    window.chrome = chrome;
+  const history = useHistory();
+
+  beforeEach(async () => {
+    localStorage.setItem('IS_LOGIN', 'YES');
+    await act(async () => {
+      render(
+        <IntlProvider locale="en" messages={en}>
+          <Router>
+            <App />
+          </Router>
+        </IntlProvider>,
+      );
+    });
   });
 
-  beforeEach(() => {
-    render(
-      <IntlProvider locale="en" messages={en}>
-        <Router>
-          <App />
-        </Router>
-      </IntlProvider>,
-    );
+  it('should render item: Export Mnemonic', async () => {
+    const element = screen.getByText('Export Mnemonic');
 
-    window.chrome = chrome;
+    expect(element).toBeInTheDocument();
   });
 
-  it('should NOT render item: Export Mnemonic', async () => {
-    const element = screen.queryByText('Export Mnemonic');
+  it('should render item: Export Private Key', async () => {
+    const element = screen.getByText('Export Private Key / Keystore');
 
-    expect(element).not.toBeInTheDocument();
+    expect(element).toBeInTheDocument();
   });
 
-  it('should NOT render item: Export Private Key', async () => {
-    const element = screen.queryByText('Export Private Key / Keystore');
+  it('should render item: Import Private Key', async () => {
+    const element = screen.getByText('Import Private Key / Keystore');
 
-    expect(element).not.toBeInTheDocument();
+    expect(element).toBeInTheDocument();
   });
 
-  it('should NOT render item: Import Private Key', async () => {
-    const element = screen.queryByText('Import Private Key / Keystore');
+  it('should render item: Manage Contacts', async () => {
+    const element = screen.getByText('Manage Contacts');
 
-    expect(element).not.toBeInTheDocument();
+    expect(element).toBeInTheDocument();
+  });
+
+  it('should render item: Manage UDTs', async () => {
+    const element = screen.getByText('Manage UDTs');
+
+    expect(element).toBeInTheDocument();
+  });
+
+  it('should render item: Manage Networks', async () => {
+    const element = screen.getByText('Manage Networks');
+
+    expect(element).toBeInTheDocument();
+  });
+
+  it('should render item: English', async () => {
+    const element = screen.getByText('English');
+
+    expect(element).toBeInTheDocument();
+  });
+
+  it('should render delete wallet form and submit', async () => {
+    const deletBtn = screen.getByText(/delete wallet/i);
+    expect(deletBtn).toBeInTheDocument();
+    userEvent.click(deletBtn);
+
+    const password = await screen.findByLabelText('Password');
+    await userEvent.type(password, 'password_1');
+    expect(screen.getByRole('form')).toHaveFormValues({
+      password: 'password_1',
+    });
+
+    const confirmBtn = screen.getByText('Confirm');
+    expect(confirmBtn).toBeInTheDocument();
+    userEvent.click(confirmBtn);
+
+    await waitFor(() => {
+      expect(browser.runtime.sendMessage).toBeCalled();
+    });
+
+    // browser.runtime.sendMessage({
+    //   type: MESSAGE_TYPE.DELETE_WALLET_ERR,
+    // });
+    // const incorrectPwd = await screen.findByText('Incorrect Password');
+    // expect(incorrectPwd).toBeInTheDocument();
+    // await waitFor(() => {
+    //   const incorrectPwd = screen.getByText('Incorrect Password');
+    //   expect(incorrectPwd).toBeInTheDocument();
+    // });
+
+    // browser.runtime.sendMessage({
+    //   type: MESSAGE_TYPE.DELETE_WALLET_OK,
+    // });
+    // expect(browser.runtime.sendMessage).toBeCalled();
+    // await waitFor(() => {
+    // expect(history.push).toBeCalled();
+    // expect(history.push).toBeCalledWith('./mnemonic-setting');
+    // });
   });
 });
