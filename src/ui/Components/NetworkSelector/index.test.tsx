e4cf445139677fa1a8dae83115959d0966861267
Harry <2411mail@gmail.com>
2020-12-16 19:53:03 +0800

    add test for NetworkSelector component

--- src/ui/Components/NetworkSelector/index.test.tsx
+++ src/ui/Components/NetworkSelector/index.test.tsx
@@ -1,18 +1,36 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import NetworkSelector from './index';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { act, render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom/extend-expect';
+import { IntlProvider } from 'react-intl';
+import en from '@common/locales/en';
+import NetworkManager from '@common/networkManager';
+import App from './index';
 
-describe('React testing library', () => {
-  let tree;
-  let container;
-  beforeEach(() => {
-    tree = render(<NetworkSelector handleNetworkChange={null} />);
-    container = tree.container;
+describe('NetworkSelector component', () => {
+  beforeEach(async () => {
+    await NetworkManager.initNetworks();
+    await act(async () => {
+      render(
+        <IntlProvider locale="en" messages={en}>
+          <Router>
+            <App handleNetworkChange={jest.fn()} />
+          </Router>
+        </IntlProvider>,
+      );
+    });
   });
 
-  it('should render title', async () => {
-    const elem = container.querySelector('#network-select');
-    expect(container).toContainElement(elem);
+  it('should render network', () => {
+    const lina = screen.getByText('Lina Mainnet');
+    expect(lina).toBeInTheDocument();
+
+    userEvent.click(lina);
+    const aggron = screen.getByText('Aggron Testnet');
+    expect(aggron).toBeInTheDocument();
+    userEvent.click(aggron);
+    const aggronAfter = screen.getAllByText('Aggron Testnet');
+    expect(aggronAfter).toHaveLength(2);
   });
 });
