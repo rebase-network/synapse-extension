d8512a039b35d6d271f9da91e7b8f935414d81a6
Harry <2411mail@gmail.com>
2020-12-16 19:39:56 +0800

    refactor modal component to dismiss lint error

--- src/ui/Components/Modal/index.tsx
+++ src/ui/Components/Modal/index.tsx
@@ -1,25 +1,24 @@
 import React from 'react';
-import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import Modal from '@material-ui/core/Modal';
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    paper: {
-      background: 'white',
-      height: '100%',
-    },
-  }),
-);
+const useStyles = makeStyles({
+  paper: {
+    background: 'white',
+    height: '100%',
+  },
+});
 
-export default function SimpleModal(props) {
+export default function SimpleModal(props: any) {
+  const { children, open, onClose } = props;
   const classes = useStyles();
-  const body = <div className={classes.paper}>{props.children}</div>;
+  const body = <div className={classes.paper}>{children}</div>;
 
   return (
     <div>
       <Modal
-        open={props.open}
-        onClose={props.onClose}
+        open={open}
+        onClose={onClose}
         aria-labelledby="simple-modal-title"
         aria-describedby="simple-modal-description"
       >
