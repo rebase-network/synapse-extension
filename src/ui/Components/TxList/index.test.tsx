29a9dc5dccde02dec640f7e067beefa49a958899
Harry <2411mail@gmail.com>
2020-12-17 13:19:39 +0800

    add test for TxList component

--- src/ui/Components/TxList/index.test.tsx
+++ src/ui/Components/TxList/index.test.tsx
@@ -1,28 +1,37 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { render, screen, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
+import userEvent from '@testing-library/user-event';
 import { IntlProvider } from 'react-intl';
 import en from '@common/locales/en';
 import { explorerUrl } from '@src/common/utils/tests/fixtures/token';
-import TxList from './index';
-import { txList } from './fixture';
+import App from './index';
+import txList from './fixtures/txList';
 
 describe('TxList', () => {
-  let tree;
-  let container;
-  let getByTestId;
-  beforeAll(() => {
-    tree = render(
+  beforeEach(() => {
+    render(
       <IntlProvider locale="en" messages={en}>
-        <TxList txList={txList} explorerUrl={explorerUrl} />
+        <Router>
+          <App txList={txList} explorerUrl={explorerUrl} />
+        </Router>
       </IntlProvider>,
     );
-    container = tree.container;
-    getByTestId = tree.getByTestId;
   });
 
   it('should render tx list', () => {
-    const elem = getByTestId('container');
-    expect(container).toContainElement(elem);
+    const list = screen.getAllByRole('list');
+    expect(list).toHaveLength(txList.length);
+  });
+
+  it('should render tx list', async () => {
+    const list = screen.getAllByRole('list');
+    userEvent.click(list[0]);
+    await waitFor(() => {
+      const hash = screen.getByText(txList[0].hash);
+
+      expect(hash).toBeInTheDocument();
+    });
   });
 });
