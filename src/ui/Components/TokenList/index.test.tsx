557e4a7571dc285c7c3ec512a82699ea06288c74
Harry <2411mail@gmail.com>
2020-12-24 21:13:45 +0800

    fix test, use testnet as current network

--- src/ui/Components/TokenList/index.test.tsx
+++ src/ui/Components/TokenList/index.test.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router, useHistory } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { act, render, screen } from '@testing-library/react';
 import { IntlProvider } from 'react-intl';
 import en from '@common/locales/en';
 import NetworkManager from '@common/networkManager';
+import { networks } from '@src/common/utils/constants/networks';
 import { explorerUrl } from '@common/utils/tests/fixtures/token';
 import App from './index';
 import udtsFixture from './fixtures/udts';
@@ -32,6 +33,7 @@ describe('token list', () => {
   beforeEach(async () => {
     await browser.storage.local.set({ udts: udtsFixture, currentWallet: currentWalletFixture });
     await NetworkManager.initNetworks();
+    await NetworkManager.setCurrentNetwork(networks[1].title);
     await act(async () => {
       render(
         <IntlProvider locale="en" messages={en}>
@@ -43,7 +45,7 @@ describe('token list', () => {
     });
   });
 
-  it('should not render any udt', async () => {
+  it('should render udt', async () => {
     const loading = screen.getByText(/2923.332 TLT/i);
     expect(loading).toBeInTheDocument();
   });
